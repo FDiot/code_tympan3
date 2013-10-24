896c40c54779d9ce047c9e2c382e98804505dcce
Anthony Truchet <anthony.truchet@logilab.fr>
2013-10-24 17:35:03 +0200

    [tests] closes #610890 - add a ground export test (known to FAIL).
    This test triggers an assertion failure : <<Trying to build a plane
    from NaN values !>>. The test is fine and the problem actually lie in
    the changeset 673b01beebb3 <<do not throw exception for invalid
    altitudes>>.
    
    Indeed this changeset was too permissive, allowing to much invalid
    data to propagate into the application. Next commit will fix that.

--- tests/data_exchange/test_SolverDataModelBuilder.cpp
+++ tests/data_exchange/test_SolverDataModelBuilder.cpp
@@ -61,7 +61,7 @@ TEST_F(BuildingFromSiteFixture, DISABLED_check_base)
     // cf. http://www.cs.ubc.ca/research/flann/ or check CGAL meshes...
 }
 
-// This test is temporaryly disable due to hard to specifiy 'good' results
+// This test uses expected bad values profided by the current implementation
 TEST_F(BuildingFromSiteFixture, check_base_known_bad_results)
 {
     load_file("../data/solver_export/base.xml");
@@ -75,7 +75,24 @@ TEST_F(BuildingFromSiteFixture, check_base_known_bad_results)
     EXPECT_EQ(5, model.num_triangles());
 }
 
+TEST_F(BuildingFromSiteFixture, check_ground)
+{
+    load_file("../data/solver_export/ground_materials.xml");
 
-// TODO TEST_F(BuildingFromSiteFixture, check_building)
+    SolverModel model;
+    SolverDataModelBuilder builder(model);
+    builder.walkTroughtSite(project->getSite());
 
-// TODO TEST_F(BuildingFromSiteFixture, check_ground)
+    EXPECT_EQ(3, model.num_materials());
+    // FIXME the default material is replicated once per triangle.
+
+    // EXPECT_EQ(6, model.num_points());
+    // TODO Not sure `how many ?` is relevant : find a better test
+
+    // How to test the altitude of a point ? or access a
+    // triangle at some place ?
+    // TODO to be completed: cf. ticket #1468184
+}
+
+// TODO cf. ticket #1474395
+// TEST_F(BuildingFromSiteFixture, check_building)
