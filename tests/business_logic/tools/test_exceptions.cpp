17834bf1d519ef9569dc6c06f39ce59ad11d0524
Anthony Truchet <anthony.truchet@logilab.fr>
2013-10-30 16:26:52 +0100

    [AltimetryBuilder] Properly report NonComparablePolygons exception - closes #1474512

--- tests/business_logic/tools/test_exceptions.cpp
+++ tests/business_logic/tools/test_exceptions.cpp
@@ -44,8 +44,8 @@ TEST(exceptions, simple_logic) {
 }
 
 TEST(exceptions, source_localized) {
-    using tympan::source_file_name;
-    using tympan::source_line_num;
+    using tympan::source_file_name_errinfo;
+    using tympan::source_line_num_errinfo;
     using boost::get_error_info;
 
     ASSERT_THROW(throw_invalid_data_localized(), tympan::exception);
@@ -54,13 +54,13 @@ TEST(exceptions, source_localized) {
     }
     catch(const std::runtime_error& exc)  {
         EXPECT_STREQ("This is bad", exc.what());
-        if( const char * const* p_filename = get_error_info<source_file_name>(exc) )
+        if( const char * const* p_filename = get_error_info<source_file_name_errinfo>(exc) )
         {
             EXPECT_STREQ(__FILE__, *p_filename);
         }
         else
             FAIL() << "We could not extract 'source_file_name' from the exception";
-        if( unsigned const* p_linenum = get_error_info<source_line_num>(exc) )
+        if( unsigned const* p_linenum = get_error_info<source_line_num_errinfo>(exc) )
         {
             EXPECT_EQ(test_exceptions_source_line_no+1, *p_linenum);
         }
