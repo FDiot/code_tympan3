2d5217114a728eeb8c00142b96a37df4772a83d7
Anthony Truchet <anthony.truchet@logilab.fr>
2014-01-23 11:28:58 +0100

    [tests] Factor out the saveAndReload function

--- tests/business_logic/infrastructure/test_road.cpp
+++ tests/business_logic/infrastructure/test_road.cpp
@@ -1,18 +1,16 @@
 //! \test test_road.cpp
 
-#include <iostream>
 
 #include "gtest/gtest.h"
 
 #include <QtXml>
 
+#include "Tympan/Tools/OSmartPtr.h"
+
 #include "Tympan/MetierSolver/DataManagerMetier/Site/TYSiteNode.h"
 #include "Tympan/MetierSolver/DataManagerMetier/Site/TYInfrastructure.h"
 #include "Tympan/MetierSolver/DataManagerMetier/EltInfrastructure/TYRoute.h"
 #include "Tympan/MetierSolver/DataManagerMetier/ComposantAcoustique/TYTrafic.h"
-
-using std::cout;
-using std::endl;
 
 LPTYSiteNode buildFlatSiteSimpleRoad(void)
 {
@@ -51,6 +49,30 @@ ASSERT_EQ(1, pInfra->getListRoute().size());
 LPTYRoute pRoad = TYRoute::safeDownCast(pInfra->getListRoute()[0]->getElement());
 }
 
+template<class ElType>
+SmartPtr<ElType> saveAndReload(SmartPtr<ElType> original,
+                               DOM_Element* p_xml=NULL)
+{
+    QDomDocument doc_xml; // Needs to be in scope for the whole function
+    DOM_Element parent_xml;
+    if(p_xml==NULL)
+    {
+        parent_xml = doc_xml.createElement("saveAndReload");
+        doc_xml.appendChild(parent_xml);
+    }
+    else
+        parent_xml = *p_xml;
+    // Actually serialise
+    DOM_Element original_xml = original->toXML(parent_xml);
+    SmartPtr<ElType> pLoaded = new ElType();
+    // Actually deserialise
+    int status = pLoaded->fromXML(original_xml);
+    if(status==1)
+        return pLoaded;
+    else
+        return SmartPtr<ElType>(); // NULL smart pointer denotes failure
+}
+
 
 TEST(TestRoads, xml_roundtrip)
 {
@@ -60,16 +82,9 @@ pRoad->road_traffic.surfaceType = RoadSurface_DR1;
 pRoad->road_traffic.surfaceAge = 10;
 pRoad->road_traffic.ramp = 0.3;
 
-QDomDocument doc_xml;
-DOM_Element parent_xml = doc_xml.createElement("whatever");
-DOM_Element road_xml = pRoad->toXML(parent_xml);
+LPTYRoute pLoadedRoad = saveAndReload(pRoad);
+ASSERT_NE(LPTYRoute(), pLoadedRoad); // Success of saveAndReload
 
-LPTYRoute pLoadedRoad = new TYRoute();
-ASSERT_NE(RoadSurface_DR1, pLoadedRoad->road_traffic.surfaceType);
-DOM_Element loaded_xml;
-int status = pLoadedRoad->fromXML(road_xml);
-
-ASSERT_EQ(1, status);
 EXPECT_EQ(pRoad->road_traffic.surfaceType, pLoadedRoad->road_traffic.surfaceType);
 EXPECT_EQ(pRoad->road_traffic.ramp,        pLoadedRoad->road_traffic.ramp);
 EXPECT_EQ(pRoad->road_traffic.surfaceAge,  pLoadedRoad->road_traffic.surfaceAge);
