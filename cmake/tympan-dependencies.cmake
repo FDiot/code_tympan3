35569dfd7076f027d3dba7ba74d4ae9393ae209b
Anthony Truchet <anthony.truchet@logilab.fr>
2014-02-17 10:35:16 +0100

    [cmake] Remove renaming of the NMPB08 dynamic library

--- cmake/tympan-dependencies.cmake
+++ cmake/tympan-dependencies.cmake
@@ -141,10 +141,11 @@ link_directories(${NMPB2008_LIBRARY_DIR})
 if(MSVC)
   # It is required to install the prebuilt NMPB08 DLL on windows
   # along the executable and under the name `RoadEmissionNMPB.dll`
-  install(FILES ${NMPB2008_INSTALL_DIR}/RoadEmissionNMPB2008.dll
-    DESTINATION . RENAME RoadEmissionNMPB.dll )
+  install(FILES ${NMPB2008_INSTALL_DIR}/RoadEmissionNMPB.dll
+    DESTINATION .)
+  list(APPEND TYMPAN_3RDPARTY_DLL_DIRS ${NMPB2008_INSTALL_DIR})
 else()
-  set(filename ${CMAKE_SHARED_LIBRARY_PREFIX}RoadEmissionNMPB2008${CMAKE_SHARED_LIBRARY_SUFFIX})
+  set(filename ${CMAKE_SHARED_LIBRARY_PREFIX}RoadEmissionNMPB${CMAKE_SHARED_LIBRARY_SUFFIX})
   install(FILES ${NMPB2008_INSTALL_DIR}/lib/${filename} DESTINATION lib)
   set(filename)
 endif(MSVC)
