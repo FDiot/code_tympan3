baf3361122cb89a52c635225ceb61bde78dfd049
Anthony Truchet <anthony.truchet@logilab.fr>
2014-03-04 12:02:02 +0100

    [tests,ToolsMetier] graft pretty-printing utilities from branch logilab
    (grafted from ba05a93ecb8710d2e7eaa1d7aafa5f75c16bd913)
    (grafted from 6cb98ef8d3d9ddf29ee5a0a243dc89b86a605dec)
    ran astyle

---
+++ Tympan/MetierSolver/ToolsMetier/prettyprint.hpp
@@ -0,0 +1,37 @@
+/**
+ * @file prettyprint.hpp
+ *
+ * @brief Utilities to pretty-print value
+ *
+ */
+
+#ifndef TYMPAN__PRETTYPRINT_HPP__INCLUDED
+#define TYMPAN__PRETTYPRINT_HPP__INCLUDED
+
+#include <cstring>
+#include <ostream>
+
+#include "Tympan/MetierSolver/ToolsMetier/OPoint3D.h"
+#include "Tympan/MetierSolver/ToolsMetier/OVector3D.h"
+#include "Tympan/MetierSolver/ToolsMetier/OPlan.h"
+#include "Tympan/MetierSolver/ToolsMetier/OSpectre.h"
+
+// Beware : Stuff::operator<< must be defined in the same namespace as Stuff
+
+// For GTest related pretty-printing please see :
+// http://code.google.com/p/googletest/wiki/AdvancedGuide#Teaching_Google_Test_How_to_Print_Your_Values
+
+// For boost::exception diagnostic please see:
+// http://www.boost.org/doc/libs/1_54_0/libs/exception/doc/diagnostic_information.html
+
+namespace tympan {
+
+} // namespace tympan
+
+::std::ostream& operator<<(::std::ostream& os, const OCoord3D& c);
+::std::ostream& operator<<(::std::ostream& os, const OVector3D& v);
+::std::ostream& operator<<(::std::ostream& os, const OPoint3D& v);
+::std::ostream& operator<<(::std::ostream& os, const OPlan& p);
+::std::ostream& operator<<(::std::ostream& os, const OSpectre& s);
+
+#endif // TYMPAN__PRETTYPRINT_HPP__INCLUDED
