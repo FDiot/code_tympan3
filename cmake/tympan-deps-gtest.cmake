83546172db2a451834f93aed9e55d15513cbf5e5
Anthony Truchet <anthony.truchet@logilab.fr>
2014-04-01 17:27:08 +0200

    [cmake] No longer build GTest in the source directory
    related to #1571184

--- cmake/tympan-deps-gtest.cmake
+++ cmake/tympan-deps-gtest.cmake
@@ -1,17 +1,21 @@
 # Add the Google Test project as an external project.
 ExternalProject_Add(GTest
   URL ${TYMPAN_3RDPARTY_GTEST}
-  SOURCE_DIR "${PROJECT_SOURCE_DIR}/3rdparty/gtest-1.6.0"
-
+  URL_MD5 ${TYMPAN_3RDPARTY_GTEST_MD5}
   # Configuration step. GTest doc said "Use shared (DLL) run-time lib even when
   # Google Test is built as static lib."
-  CMAKE_ARGS -Dgtest_force_shared_crt=ON
-  # Installation step. Disable install step.
+  CMAKE_ARGS -Dgtest_force_shared_crt=ON  
   INSTALL_COMMAND ""
+  BUILD_IN_SOURCE 0
 )
-ExternalProject_Get_Property (GTest SOURCE_DIR BINARY_DIR)
-# SOURCE_DIR is related to the GTest project.
-include_directories (${SOURCE_DIR}/include)
+ExternalProject_Get_Property(GTest SOURCE_DIR)
+set(GTEST_SOURCE_DIR ${SOURCE_DIR})
+set(GTEST_INCLUDE_DIR ${GTEST_SOURCE_DIR}/include)
+
+include_directories (${GTEST_INCLUDE_DIR})
 #NB Adding GTest include in the non-test tree is to enable gtest_prod.h
 
-# TODO : install GTest and use it from the install dir instead of explicit source dir
+if(TYMPAN_DEBUG_CMAKE)
+message(STATUS "INFO GTEST_INCLUDE_DIR: " "${GTEST_INCLUDE_DIR}" )
+
+endif()
