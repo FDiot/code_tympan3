e5ef6da65320be229accf8789f77c2efe6ee1601
Laura M?dioni <laura.medioni@logilab.fr>
2014-04-09 09:34:47 +0200

    Stop allocating memory for the C++ instances referenced on the python side
    - The T* wrapped in the smart pointers are allocated on the C++ side.
    It doesn't make sense to new them on the python side and then rewrite their
    pointer value with the one of a C+ allocated object without deallocating them.
    
    - ResultModel and ProblemModel should hold a pointer to the C++ instances in
      order not to allocate memory twice
    
    - Add some NULL pointer check accordingly to make sure we don't try to use non
    allocated objects on the python side.

--- python/src/pytam.pyx
+++ python/src/pytam.pyx
@@ -1,12 +1,16 @@
 from libcpp cimport bool
 from libcpp.vector cimport vector
-cimport pytam
+
+class NullCppObject(Exception):
+    """
+    The SmartPtr object is empty
+    """
 
 def loadsolver(foldername, Computation comp):
     load_solver(foldername, comp.thisptr.getRealPointer())
 
 cdef class ProblemModel:
-    cdef AcousticProblemModel thisptr
+    cdef AcousticProblemModel *thisptr
     def __cinit__(self):
         pass
     def nbpoints(self):
@@ -19,14 +23,14 @@ cdef class ProblemModel:
         self.thisptr.export_triangles_soup(filename)
 
 cdef class ResultModel:
-    cdef AcousticResultModel thisptr
+    cdef AcousticResultModel *thisptr
     def __cinit__(self):
         pass
 
 cdef class SolverModelBuilder:
     cdef SolverDataModelBuilder *thisptr
     def __cinit__(self, ProblemModel model):
-        self.thisptr = new SolverDataModelBuilder (model.thisptr)
+        self.thisptr = new SolverDataModelBuilder (model.thisptr[0])
     def fill_problem(self, Site site):
         self.thisptr.walkTroughtSite(site.thisptr)
 
@@ -38,24 +42,27 @@ cdef class ElementArray:
 cdef class Element:
     cdef SmartPtr[TYElement] thisptr
     def __cinit__(self):
-        self.thisptr = SmartPtr[TYElement](new TYElement())
+        self.thisptr = SmartPtr[TYElement]()
     def name(self):
+        if self.thisptr.getRealPointer() == NULL:
+            raise NullCppObject()
         return self.thisptr.getRealPointer().getName().toStdString()
 
 cdef class Site:
     cdef SmartPtr[TYSiteNode] thisptr
     def __cinit__(self):
-        self.thisptr = SmartPtr[TYSiteNode](new TYSiteNode())
+        self.thisptr = SmartPtr[TYSiteNode]()
     def childs(self):
-        # Retrieves the childs of the site (LPTYElements) into the "childs" vector.
+        if self.thisptr.getRealPointer() == NULL:
+            raise NullCppObject()
         cdef vector[SmartPtr[TYElement]] childs
+        # Retrieves the childs of the site (LPTYElements) into the "childs" vector.
         self.thisptr.getRealPointer().getChilds(childs, False)
         # Constructs a python list and appends to it the LPTYElement objects
         # previously wrapped into Elements objects
         pylist = []
-        cdef Element child
         for i in range(childs.size()):
-            child = Element.__new__(Element)
+            child = Element()
             child.thisptr = childs[i]
             pylist.append(child)
         return pylist
@@ -63,16 +70,22 @@ cdef class Site:
 cdef class Computation:
     cdef SmartPtr[TYCalcul] thisptr
     def __cinit__(self):
-        self.thisptr = SmartPtr[TYCalcul](new TYCalcul())
+        self.thisptr = SmartPtr[TYCalcul]()
     def go(self):
+        if self.thisptr.getRealPointer() == NULL:
+            raise NullCppObject()
         return self.thisptr.getRealPointer().go()
     def problem(self):
-        cdef ProblemModel problem = ProblemModel.__new__(ProblemModel)
-        problem.thisptr = self.thisptr.getRealPointer()._acousticProblem
+        if self.thisptr.getRealPointer() == NULL:
+            raise NullCppObject()
+        problem = ProblemModel()
+        problem.thisptr = &self.thisptr.getRealPointer()._acousticProblem
         return problem
     def result(self):
-        cdef ResultModel result = ResultModel.__new__(ResultModel)
-        result.thisptr = self.thisptr.getRealPointer()._acousticResult
+        if self.thisptr.getRealPointer() == NULL:
+            raise NullCppObject()
+        result = ResultModel()
+        result.thisptr = &self.thisptr.getRealPointer()._acousticResult
         return result
 
 cdef class Project:
@@ -82,11 +95,15 @@ cdef class Project:
     # Constructs a Computation python object and sets the wrapped instance
     # with the Computation retrieved from the C++ LPTYProject.
     def current_computation(self):
-        cdef Computation comp = Computation.__new__(Computation)
+        if self.thisptr.getRealPointer() == NULL:
+            raise NullCppObject()
+        comp = Computation()
         comp.thisptr = self.thisptr.getRealPointer().getCurrentCalcul()
         return comp
     def site(self):
-        cdef Site site =  Site.__new__(Site)
+        if self.thisptr.getRealPointer() == NULL:
+            raise NullCppObject()
+        site = Site()
         site.thisptr = self.thisptr.getRealPointer().getSite()
         return site
     @staticmethod
@@ -102,3 +119,4 @@ cdef class Project:
         pproj = project.thisptr.getRealPointer()
         pproj = safeDownCast(el.thisptr.getRealPointer())
         return project
+
