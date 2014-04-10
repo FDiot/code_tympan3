11a400bd30bc3f9b86fcc8cb9a8f58dd000d6b7c
Laura M?dioni <laura.medioni@logilab.fr>
2014-04-10 08:42:30 +0200

    Pure python mode
    use decorators and remove cdef declarations

--- python/src/pytam.pyx
+++ python/src/pytam.pyx
@@ -2,87 +2,109 @@ from libcpp cimport bool
 from libcpp.vector cimport vector
 from libcpp.deque cimport deque
 from cython.operator cimport dereference as deref, preincrement as inc
+import cython
 
 class NullCppObject(Exception):
     """
     The referenced C++ object is NULL
     """
 
-def loadsolver(foldername, Computation comp):
+@cython.locals(comp=Computation)
+def loadsolver(foldername, comp):
     load_solver(foldername, comp.thisptr.getRealPointer())
 
 cdef class ProblemModel:
-    cdef AcousticProblemModel *thisptr
+    thisptr = cython.declare(cython.pointer(AcousticProblemModel))
+
     def __cinit__(self):
         self.thisptr = NULL
+
     def nbpoints(self):
         if self.thisptr == NULL:
             raise NullCppObject()
         return self.thisptr.num_points()
+
     def nbtriangles(self):
         if self.thisptr == NULL:
             raise NullCppObject()
         return self.thisptr.num_triangles()
+
     def nbmaterials(self):
         if self.thisptr == NULL:
             raise NullCppObject()
         return self.thisptr.num_materials()
+
     def export_triangles(self, filename):
         if self.thisptr == NULL:
             raise NullCppObject()
         self.thisptr.export_triangles_soup(filename)
 
+
 cdef class ResultModel:
-    cdef AcousticResultModel *thisptr
+    thisptr = cython.declare(cython.pointer(AcousticResultModel))
     def __cinit__(self):
         pass
 
+
 cdef class SolverModelBuilder:
-    cdef SolverDataModelBuilder *thisptr
-    cdef AcousticProblemModel *model
+    thisptr = cython.declare(cython.pointer(SolverDataModelBuilder))
+    model = cython.declare(cython.pointer(AcousticProblemModel))
+
     def __cinit__(self):
         self.model = new AcousticProblemModel()
         self.thisptr = new SolverDataModelBuilder (self.model[0])
-    def fill_problem(self, Site site):
+
+    @cython.locals(site=Site)
+    def fill_problem(self, site):
         self.process_infrastructure(site)
         self.process_altimetry(site)
+
     def problem_model(self):
         problem = ProblemModel()
         problem.thisptr = self.model
         return problem
-    def process_infrastructure(self, Site site):
-        cdef vector[bool] is_screen_face_idx
-        cdef vector[SmartPtr[TYGeometryNode]] face_list
-        cdef TYElement *pelt
-        cdef TYGeometryNode geonode
-        cdef unsigned int nb_building_faces = 0
+
+    @cython.locals(site=Site)
+    def process_infrastructure(self, site):
+        is_screen_face_idx = cython.declare(vector[bool])
+        face_list = cython.declare(vector[SmartPtr[TYGeometryNode]])
+        pelt = cython.declare(cython.pointer(TYElement))
+        geonode = cython.declare(TYGeometryNode)
+        nb_building_faces = cython.declare(cython.uint)
+        nb_building_faces = 0
         site.thisptr.getRealPointer().getListFaces(face_list, nb_building_faces,
                            is_screen_face_idx)
-        cdef TYAcousticSurface *pSurf = NULL
+        pSurf = cython.declare(cython.pointer(TYAcousticSurface))
         for i in range(nb_building_faces):
             pelt = face_list[i].getRealPointer().getElement()
             pSurf = safeDownCast(pelt)
             if pSurf != NULL:
                 geonode = (face_list[i].getRealPointer())[0]
                 self.thisptr.setAcousticTriangle(geonode)
-    def process_altimetry(self, Site site):
-        cdef deque[OPoint3D] points
-        cdef deque[OTriangle] triangles
-        cdef deque[SmartPtr[TYSol]] materials
-        cdef TYTopographie *ptopo = site.thisptr.getRealPointer().getTopographie().getRealPointer()
-        cdef UuidAdapter *element_uid = new UuidAdapter(ptopo.getID())
+
+    @cython.locals(site=Site)
+    def process_altimetry(self, site):
+        points = cython.declare(deque[OPoint3D])
+        triangles = cython.declare(deque[OTriangle])
+        materials = cython.declare(deque[SmartPtr[TYSol]])
+        ptopo = cython.declare(cython.pointer(TYTopographie))
+        ptopo = site.thisptr.getRealPointer().getTopographie().getRealPointer()
+        element_uid = cython.declare(cython.pointer(UuidAdapter))
+        element_uid = new UuidAdapter(ptopo.getID())
         ptopo.exportMesh(points, triangles, &materials)
         # process mesh
-        cdef size_t nbpoints = points.size()
-        cdef vector[size_t] map_to_model_node_idx = vector[size_t] (nbpoints)
-        cdef deque[OPoint3D].iterator itp = points.begin()
-        cdef deque[OTriangle].iterator itt = triangles.begin()
-        cdef unsigned int i = 0
+        map_to_model_node_idx = cython.declare(vector[size_t])
+        map_to_model_node_idx = vector[size_t] (points.size())
+        itp = cython.declare(deque[OPoint3D].iterator)
+        itp = points.begin()
+        i = 0
         while itp != points.end():
             # Add the points
             map_to_model_node_idx[i] = self.model.make_node(deref(itp))
             i = i+1
             inc(itp)
+        itt = cython.declare(deque[OTriangle].iterator)
+        itt = triangles.begin()
         while  itt != triangles.end():
             # Assert consistency of the OPoint3D given in the mesh
             assert (deref(itt)._A == points[deref(itt)._p1])
@@ -94,9 +116,9 @@ cdef class SolverModelBuilder:
                                      map_to_model_node_idx[deref(itt)._p3])
             inc(itt)
         # make material
-        cdef AcousticTriangle *actri = NULL
-        cdef TYSol *psol
-        cdef shared_ptr[AcousticMaterialBase] pmat
+        actri = cython.declare(cython.pointer(AcousticTriangle))
+        psol = cython.declare(cython.pointer(TYSol))
+        pmat = cython.declare(shared_ptr[AcousticMaterialBase])
         for i in range(triangles.size()):
             # XXX find a way to use triangle() instead of ptriangle()
             actri = self.model.ptriangle(i)
@@ -106,28 +128,35 @@ cdef class SolverModelBuilder:
                                             psol.getResistivite())
             actri.made_of = pmat
 
+
 cdef class ElementArray:
-    cdef vector[SmartPtr[TYElement]] thisptr
+    thisptr = cython.declare(vector[SmartPtr[TYElement]])
     def __cinit__(self):
         pass
 
+
 cdef class Element:
-    cdef SmartPtr[TYElement] thisptr
+    thisptr = cython.declare(SmartPtr[TYElement])
+
     def __cinit__(self):
         self.thisptr = SmartPtr[TYElement]()
+
     def name(self):
         if self.thisptr.getRealPointer() == NULL:
             raise NullCppObject()
         return self.thisptr.getRealPointer().getName().toStdString()
 
+
 cdef class Site:
-    cdef SmartPtr[TYSiteNode] thisptr
+    thisptr = cython.declare(SmartPtr[TYSiteNode])
+
     def __cinit__(self):
         self.thisptr = SmartPtr[TYSiteNode]()
+
     def childs(self):
         if self.thisptr.getRealPointer() == NULL:
             raise NullCppObject()
-        cdef vector[SmartPtr[TYElement]] childs
+        childs = cython.declare(vector[SmartPtr[TYElement]])
         # Retrieves the childs of the site (LPTYElements) into the "childs" vector.
         self.thisptr.getRealPointer().getChilds(childs, False)
         # Constructs a python list and appends to it the LPTYElement objects
@@ -139,45 +168,53 @@ cdef class Site:
             pylist.append(child)
         return pylist
 
+
 cdef class Computation:
-    cdef SmartPtr[TYCalcul] thisptr
+    thisptr = cython.declare(SmartPtr[TYCalcul])
+
     def __cinit__(self):
         self.thisptr = SmartPtr[TYCalcul]()
+
     def go(self):
         if self.thisptr.getRealPointer() == NULL:
             raise NullCppObject()
         return self.thisptr.getRealPointer().go()
+
     def problem(self):
         if self.thisptr.getRealPointer() == NULL:
             raise NullCppObject()
         problem = ProblemModel()
-        problem.thisptr = &self.thisptr.getRealPointer()._acousticProblem
+        problem.thisptr = cython.address(self.thisptr.getRealPointer()._acousticProblem)
         return problem
+
     def result(self):
         if self.thisptr.getRealPointer() == NULL:
             raise NullCppObject()
         result = ResultModel()
-        result.thisptr = &self.thisptr.getRealPointer()._acousticResult
+        result.thisptr = cython.address(self.thisptr.getRealPointer()._acousticResult)
         return result
 
+
 cdef class Project:
-    cdef SmartPtr[TYProjet] thisptr
+    thisptr = cython.declare(SmartPtr[TYProjet])
+
     def __cinit__(self):
         self.thisptr = SmartPtr[TYProjet]()
-    # Constructs a Computation python object and sets the wrapped instance
-    # with the Computation retrieved from the C++ LPTYProject.
+
     def current_computation(self):
         if self.thisptr.getRealPointer() == NULL:
             raise NullCppObject()
         comp = Computation()
         comp.thisptr = self.thisptr.getRealPointer().getCurrentCalcul()
         return comp
+
     def site(self):
         if self.thisptr.getRealPointer() == NULL:
             raise NullCppObject()
         site = Site()
         site.thisptr = self.thisptr.getRealPointer().getSite()
         return site
+
     @staticmethod
     def from_xml(filepath):
         project = Project()
@@ -187,6 +224,7 @@ cdef class Project:
             return project
         else:
             raise Exception("Project could not be loaded from %s" % filepath)
+
     def to_xml(self, filepath):
         if self.thisptr.getRealPointer() != NULL:
             save_project(filepath, self.thisptr)
