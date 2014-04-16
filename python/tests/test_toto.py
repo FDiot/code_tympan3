3a9a843c3f16d7d0249b20ae7f675b2654a4007d
Anthony Truchet <anthony.truchet@logilab.fr>
2014-04-16 15:03:35 +0200

    [cmake, python] Integrate python test with CTest
    NB The test is expected to fail because the PYTHONPATH is not set
       The test driver environment is configured in a forthcomming commit

--- python/tests/test_toto.py
+++ python/tests/test_toto.py
@@ -1,12 +1,16 @@
 import sys, os
 
-print sys.path
-print os.environ.get('PYTHONPATH', "")
+try:
+    import toto
+except ImportError as exc:
+    # Add information regarding PYTHONPATH to the exception
+    exc.args = exc.args + ('PYTHONPATH=%s' % os.environ.get('PYTHONPATH', ''),)
+    raise
 
-import toto
 
 # Element constructed on the Python side
 print "** Test 1 ...**"
 elt = toto.PyMyElement ("toto")
+# NB An AssertionError will exit the interpreter with status code 1
 assert elt.get_name() == "toto"
 print "** Test 1 OK **"
