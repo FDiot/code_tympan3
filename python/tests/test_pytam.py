aea2859ca99aa9b10954d4181c9c3816f2b1ab62
Anthony Truchet <anthony.truchet@logilab.fr>
2014-04-18 17:43:38 +0200

    [python, tests] Make the path to the test data and plugins portable

--- python/tests/test_pytam.py
+++ python/tests/test_pytam.py
@@ -1,15 +1,33 @@
-import pytam
+import os, os.path as osp
+
 import unittest
+
+import pytam
+
+_HERE = osp.realpath(osp.dirname(__file__))
+
+_PROJECT_BASE = osp.abspath(osp.join(_HERE, '..', '..'))
+
+_TEST_DATA_DIR = osp.join(_PROJECT_BASE, 'tests', 'data')
+assert osp.isdir(_TEST_DATA_DIR), "The test data dir does not exists '%s'" % _TEST_DATA_DIR
+
+_TEST_SOLVERS_DIR = osp.join(_PROJECT_BASE, 'pluginsd')
+if not osp.isdir(_TEST_SOLVERS_DIR):
+    solver_dir = osp.abspath(osp.join(_HERE, '..', '..', 'plugins'))
+assert osp.isdir(_TEST_SOLVERS_DIR), "The test solver plugins dir does not exists '%s'" % _TEST_SOLVERS_DIR
+
 
 class TestTympan(unittest.TestCase):
     def test_solve(self):
-        project = pytam.Project.from_xml("data/projects/10_PROJET_SITE_emprise_non_convexe_avec_butte_et_terrains.xml")
+        project = pytam.Project.from_xml(osp.join(_TEST_DATA_DIR, 'projects-panel',
+                    "10_PROJET_SITE_emprise_non_convexe_avec_butte_et_terrains.xml"))
         computation = project.current_computation()
-        pytam.loadsolver("data/solver", computation)
+        pytam.loadsolver(_TEST_SOLVERS_DIR, computation)
         self.assertTrue(computation.go())
 
     def test_hierarchy(self):
-        project = pytam.Project.from_xml("data/projects/10_PROJET_SITE_emprise_non_convexe_avec_butte_et_terrains.xml")
+        project = pytam.Project.from_xml(osp.join(_TEST_DATA_DIR, 'projects-panel',
+                    "10_PROJET_SITE_emprise_non_convexe_avec_butte_et_terrains.xml"))
         site = project.site()
         childs = site.childs()
         for c in childs:
@@ -18,7 +36,7 @@ class TestTympan(unittest.TestCase):
     def test_base(self):
         # XXX This test uses expected bad values provided by the current
         # implementation
-        project = pytam.Project.from_xml("data/projects/base.xml")
+        project = pytam.Project.from_xml(osp.join(_TEST_DATA_DIR, 'solver_export', "base.xml"))
         model = project.current_computation().problem()
         builder = pytam.SolverModelBuilder(model)
         builder.fill_problem(project.site())
@@ -32,7 +50,7 @@ class TestTympan(unittest.TestCase):
 
 
     def test_triangles(self):
-        project = pytam.Project.from_xml("data/projects/tiny_site.xml")
+        project = pytam.Project.from_xml(osp.join(_TEST_DATA_DIR, "tiny_site.xml"))
         model = project.current_computation().problem()
         builder = pytam.SolverModelBuilder(model)
         builder.fill_problem(project.site())
@@ -40,13 +58,13 @@ class TestTympan(unittest.TestCase):
 
     @unittest.skip("Implementation to be fixed")
     def test_ground_materials(self):
-        project = pytam.Project.from_xml("data/projects/ground_materials.xml")
+        project = pytam.Project.from_xml(osp.join(_TEST_DATA_DIR, 'solver_export', "ground_materials.xml"))
         model = project.current_computation().problem()
         builder = pytam.SolverModelBuilder(model)
         builder.fill_problem(project.site())
         self.assertEqual(model.nbmaterials(), 3)
         # XXX FIXME: the default material is replicated once per triangle
-        # TODO to be completed: cf. ticket #1468184 
+        # TODO to be completed: cf. ticket #1468184
 
 if __name__ == '__main__':
     unittest.main()
