e9a5af378a5feef635519c8b029baaa1e34ea2c8
Anthony Truchet <anthony.truchet@logilab.fr>
2014-05-14 19:01:15 +0200

    [python, tests] Factor out paths configuration code
    Put paths configuration utilities in the
    ``tympan_config_paths.py`` script :
    
    * allow to access the test data, solvers and other resources in a
      cross platform way (the relative path to use between a test
      executable and the resources is not the same on Linux and Windows)
    * handle the connfiguration of sys.path to find either the Debug
      or Release the cython module

--- python/tests/test_toto.py
+++ python/tests/test_toto.py
@@ -1,21 +1,7 @@
-import sys, os, os.path as osp
-
-_HERE = osp.realpath(osp.dirname(__file__))
-_PROJECT_BASE = osp.abspath(osp.join(_HERE, '..', '..'))
-
-_CYTHON_EXTENSION_DIR = {
-    'Release' : osp.abspath(osp.join(_PROJECT_BASE, 'python', 'cython')),
-    'Debug'   : osp.abspath(osp.join(_PROJECT_BASE, 'python', 'cython_d'))
-}
-
-config = len(sys.argv) > 1 and sys.argv[1]
-if config in _CYTHON_EXTENSION_DIR:
-        sys.path.append(_CYTHON_EXTENSION_DIR[config])
-else:
-    print 'WARNING: no config specification or unknown config (%s)' % config
-
+import os
 
 try:
+    import tympan_config_paths
     import toto
 except ImportError as exc:
     # Add information regarding PYTHONPATH to the exception
