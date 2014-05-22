da005400ef7968c64d29e3518a6a73428999b1a8
Anthony Truchet <anthony.truchet@logilab.fr>
2014-05-22 10:47:04 +0200

    [cgal] Complete the cgal_bridge module
    Ensure the triangulation of concave polygon is OK.
    
    related to #1627957

--- tests/business_logic/tools/test_cgal_bridge.cpp
+++ tests/business_logic/tools/test_cgal_bridge.cpp
@@ -27,10 +27,10 @@ using std::deque;
 #include "Tympan/MetierSolver/CommonTools/prettyprint.hpp"
 #include "Tympan/MetierSolver/DataManagerMetier/cgal_bridge.hpp"
 
+using tympan::ITYPolygonTriangulator;
 
 TEST(TYPolygonTriangulatorTest, instanciation)
 {
-    using tympan::ITYPolygonTriangulator;
     TYPolygon poly;
 
     auto p_triangulator = tympan::make_polygon_triangulator(poly);
@@ -39,8 +39,6 @@ TEST(TYPolygonTriangulatorTest, instanciation)
 
 TEST(TYPolygonTriangulatorTest, single_triangle)
 {
-    using tympan::ITYPolygonTriangulator;
-
     // NB New c++'11 initializer-list syntax
     // Cf. http://en.wikipedia.org/wiki/C++11#Initializer_lists
     vector<TYPoint> pts { {0,0,0}, {0,1,0}, {1, 0, 0}};
@@ -54,4 +52,21 @@ TEST(TYPolygonTriangulatorTest, single_triangle)
 
     ASSERT_EQ(3, points.size());
     ASSERT_EQ(1, triangles.size());
+}
+
+TEST(TYPolygonTriangulatorTest, concave_quadrangle)
+{
+    // NB New c++'11 initializer-list syntax
+    // Cf. http://en.wikipedia.org/wiki/C++11#Initializer_lists
+    vector<TYPoint> pts { {0,0,0}, {5, 0,0}, {1, 1, 0}, {0.5, 6, 0}};
+    TYPolygon poly(pts);
+
+    deque<OPoint3D> points;
+    deque<OTriangle> triangles;
+
+    auto p_triangulator = tympan::make_polygon_triangulator(poly);
+    p_triangulator->exportMesh(points, triangles);
+
+    ASSERT_EQ(4, points.size());
+    ASSERT_EQ(2, triangles.size());
 }
