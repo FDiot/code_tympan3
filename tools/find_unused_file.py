5afa94fe2bb5626dff2484a198b213ad809f88ff
Sylvain Th?nault <sylvain.thenault@logilab.fr>
2014-05-22 17:00:17 +0200

    [tools] small script attempting to detect dead files (not included)

---
+++ tools/find_unused_file.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+"""usage: ./tools/find_unused_file.py  Tympan/
+
+(working directory matters)
+"""
+import sys
+import os
+from os.path import join, basename
+
+h_files = set()
+included_files = set()
+
+for directory, dirs, files in os.walk(sys.argv[1]):
+    for fname in files:
+        if fname.endswith(('.h', '.hpp')):
+            h_files.add(join(directory, fname))
+            #print 'H file', join(directory, fname)
+        if fname.endswith(('.h', '.hpp', '.cpp')):
+            for line in open(join(directory, fname)):
+                line = line.strip()
+                if line.startswith('#include "'):
+                    included = line.split('"')[1]
+                    if fname.split('.', 1)[0] != included.split('.', 1)[0]:
+                        included_files.add(included)
+                        #print 'include', included
+
+not_found = set()
+for file_path in included_files:
+    try:
+        h_files.remove(file_path)
+    except KeyError:
+        not_found.add(file_path)
+
+for file_path in tuple(h_files):
+    if basename(file_path) in not_found:
+        h_files.remove(file_path)
+
+for file_path in sorted(h_files):
+    print file_path, 'seems not used'
