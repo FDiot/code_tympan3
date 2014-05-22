f1c67d5ff388f13bdeeb5310c672d8c415faf27a
Sylvain Th?nault <sylvain.thenault@logilab.fr>
2014-05-22 15:25:47 +0200

    [python test] silent output of test_pytam

--- python/tests/test_pytam.py
+++ python/tests/test_pytam.py
@@ -4,11 +4,14 @@ import unittest
 
 import numpy as np
 
-import pytam
+from utils import TEST_DATA_DIR, TEST_SOLVERS_DIR, no_output
 
-from utils import TEST_DATA_DIR, TEST_SOLVERS_DIR
+with no_output():
+    import pytam
+
 def load_project(*path):
-    return pytam.Project.from_xml(osp.join(TEST_DATA_DIR, *path))
+    with no_output():
+        return pytam.Project.from_xml(osp.join(TEST_DATA_DIR, *path))
 
 
 class TestTympan(unittest.TestCase):
@@ -17,8 +20,9 @@ class TestTympan(unittest.TestCase):
         project = load_project('projects-panel',
                                "10_PROJET_SITE_emprise_non_convexe_avec_butte_et_terrains.xml")
         computation = project.current_computation()
-        pytam.loadsolver(TEST_SOLVERS_DIR, computation)
-        self.assertTrue(computation.go())
+        with no_output():
+            pytam.loadsolver(TEST_SOLVERS_DIR, computation)
+            self.assertTrue(computation.go())
 
     def test_hierarchy(self):
         project = load_project('projects-panel',
@@ -32,9 +36,10 @@ class TestTympan(unittest.TestCase):
         # XXX This test uses expected bad values provided by the current
         # implementation
         project = load_project('solver_export', "base.xml")
-        model = project.current_computation().acoustic_problem()
-        builder = pytam.SolverModelBuilder(model)
-        builder.fill_problem(project.site())
+        with no_output():
+            model = project.current_computation().acoustic_problem()
+            builder = pytam.SolverModelBuilder(model)
+            builder.fill_problem(project.site())
         self.assertEqual(model.nbpoints(), 6) # OK
         self.assertEqual(model.nbtriangles(), 5) # XXX should be 4
         self.assertEqual(model.nbmaterials(), 5) # XXX should be 1
@@ -50,12 +55,13 @@ class TestTympan(unittest.TestCase):
         # load a xml project, build an acoustic problem from it and retrieve
         # its triangular mesh to make sure it contains the correct data
         project = load_project("tiny_site.xml")
-        model = project.current_computation().acoustic_problem()
-        builder = pytam.SolverModelBuilder(model)
-        builder.fill_problem(project.site())
-        # exports in nodes_test the nodes coordinates (x,y,z) and in triangles_test
-        # the triangle nodes indices (position in the nodes_test array)
-        (nodes_test, triangles_test) = model.export_triangular_mesh()
+        with no_output():
+            model = project.current_computation().acoustic_problem()
+            builder = pytam.SolverModelBuilder(model)
+            builder.fill_problem(project.site())
+            # exports in nodes_test the nodes coordinates (x,y,z) and in triangles_test
+            # the triangle nodes indices (position in the nodes_test array)
+            (nodes_test, triangles_test) = model.export_triangular_mesh()
         nodes_ref = np.loadtxt(osp.join(TEST_DATA_DIR, "expected",
                                         "test_mesh_nodes_ref.csv"),
                                delimiter=';', dtype=np.float)
@@ -71,9 +77,10 @@ class TestTympan(unittest.TestCase):
     @unittest.skip("Implementation to be fixed")
     def test_ground_materials(self):
         project = load_project('solver_export', "ground_materials.xml")
-        model = project.current_computation().acoustic_problem()
-        builder = pytam.SolverModelBuilder(model)
-        builder.fill_problem(project.site())
+        with no_output():
+            model = project.current_computation().acoustic_problem()
+            builder = pytam.SolverModelBuilder(model)
+            builder.fill_problem(project.site())
         self.assertEqual(model.nbmaterials(), 3)
         # XXX FIXME: the default material is replicated once per triangle
         # TODO to be completed: cf. ticket #1468184
