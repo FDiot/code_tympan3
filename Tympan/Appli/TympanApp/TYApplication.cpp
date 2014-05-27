944697d8293db79d33974c2eb06a6e8f76abe490
Laura M?dioni <laura.medioni@logilab.fr>
2014-05-27 11:21:43 +0200

    Drop _binaryDir attribute from TYApplication
    And use static method QCoreApplication::applicationDirPath() instead where needed
    
    This allows to remove the dependency towards TYApplication in EnvironmentUtils
    (EnvironmentUtils should be independant from TYXXX classes).

--- Tympan/Appli/TympanApp/TYApplication.cpp
+++ Tympan/Appli/TympanApp/TYApplication.cpp
@@ -171,7 +171,7 @@ void TYApplication::setCurrentDirName(const QString& newValue)
 
 
 TYApplication::TYApplication(int& argc, char** argv)
-    : QApplication(argc, argv), _actionManager(10), _binaryDir(applicationDirPath())
+    : QApplication(argc, argv), _actionManager(10)
 {
     _pSplash = NULL;
     _pMainWnd = NULL;
