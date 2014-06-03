5dcd60985e1bced5032758e82e052f0dd5821a5e
Anthony Truchet <anthony.truchet@logilab.fr>
2014-06-03 16:02:44 +0200

    [python, tests] Allow to redirect silented tests' outputs to files
    related to #1645374

--- python/tests/test_toto.py
+++ python/tests/test_toto.py
@@ -1,6 +1,11 @@
 import os
 import utils
 
+TEST_OUTPUT_REDIRECTED = 'test_toto_out.log'
+TEST_ERRORS_REDIRECTED = 'test_toto_err.log'
+# TEST_OUTPUT_REDIRECTED = os.devnull
+
+
 try:
     import toto
 except ImportError as exc:
@@ -14,7 +19,11 @@ except ImportError as exc:
 
 # Element constructed on the Python side
 print "** Test 1 ...**"
-elt = toto.PyMyElement ("toto")
+
+
+with utils.no_output(to=TEST_OUTPUT_REDIRECTED, err_to=TEST_ERRORS_REDIRECTED):
+    elt = toto.PyMyElement ("toto")
+
 # NB An AssertionError will exit the interpreter with status code 1
 assert elt.get_name() == "toto"
 print "** Test 1 OK **"
