5168a746df0331c0875d61ead170a3a718cf3ee2
Laura M?dioni <laura.medioni@logilab.fr>
2014-06-24 10:55:09 +0200

    [tests] Add tests for receptors export
    - Refactor a bit the existing test script to avoid code duplication.
    - Remove duplicated coordinates from receptor_positions_Project_28.txt
    
    File name will be changed in next cset to preserve the diff for the review
    
    Related to #1649968

--- python/tests/test_sources.py
+++ python/tests/test_sources.py
@@ -74,6 +74,39 @@ def make_sources_test_with_file(project_file, sources_file):
         assert_allclose(expected_sources, real_sources, atol=1e-03)
     return test_with_file
 
+def make_receptors_test_with_file(project_file, receptors_file):
+    """ For a N_*.xml file from data/computed-projects-panel, load
+     the corresponding project and build an acoustic problem model. Then
+     check that the receptors have correctly been exported by looking at the
+     expected result from data/expected/receptors_positions_Project_N.txt and comparing:
+        - Number of receptors
+        - Global positions of the receptors
+    """
+    def test_with_file(self):
+        """
+            Operates as in make_sources_test_with_file (see above), except here
+            we are just dealing with positions and not spetrums.
+        """
+        project = self.load_project(project_file)
+        model = project.current_computation.acoustic_problem
+        # Check no receptors are missing
+        nexpected_receptors = sum(1 for line in open(receptors_file))
+        self.assertEqual(nexpected_receptors, model.nreceptors)
+        # Read expected result
+        expected_receptors = np.loadtxt(receptors_file)
+        real_receptors = np.array(list([receptor.position.x, receptor.position.y,
+                                        receptor.position.z]
+                                       for receptor in model.receptors))
+        if model.nreceptors > 1:
+            # Order the two lists to be able to compare the receptors' positions two by two
+            expected_receptors = sorted(expected_receptors, cmp=compare_floats)
+            real_receptors = sorted(real_receptors, cmp=compare_floats)
+        else:
+            real_receptors = real_receptors[0]
+        # Compare the receptors
+        assert_allclose(expected_receptors, real_receptors, atol=1e-03)
+    return test_with_file
+
 
 # Retrieve all the available "N_*" xml files and make a test for each one
 for project_file in os.listdir(_TEST_PROBLEM_DIR):
@@ -85,11 +118,18 @@ for project_file in os.listdir(_TEST_PROBLEM_DIR):
     # Check the corresponding result files exist
     for result_file in os.listdir(TEST_RESULT_DIR):
         projnum = project_file.split('_')[0] # get project number
+        # Sources
         if result_file == 'source_positions_Project_' + projnum + '.txt':
             source_file_path = osp.join(TEST_DATA_DIR, TEST_RESULT_DIR, result_file)
             test_name = "test_sources_" + project_file.split('_', 1)[1].split('.')[0].lower()
             setattr(TestTympan, test_name, make_sources_test_with_file(project_file_path,
                                                                        source_file_path))
+        # Receptors
+        if result_file == 'receptor_positions_Project_' + projnum + '.txt':
+            receptor_file_path = osp.join(TEST_DATA_DIR, TEST_RESULT_DIR, result_file)
+            test_name = "test_receptors_" + project_file.split('_', 1)[1].split('.')[0].lower()
+            setattr(TestTympan, test_name, make_receptors_test_with_file(project_file_path,
+                                                                         receptor_file_path))
 
 if __name__ == '__main__':
     from utils import main
