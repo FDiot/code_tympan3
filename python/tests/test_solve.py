ba175fe5f5d68581132838a5829238169a5ebc73
Anthony Truchet <anthony.truchet@logilab.fr>
2014-07-01 15:17:53 +0200

    [altimetry] Create datamodel bridged with shapely
    related to #1650179

---
+++ python/tests/test_solve.py
@@ -0,0 +1,77 @@
+import os, os.path as osp
+import unittest
+
+import numpy as np
+
+from utils import (TEST_SOLVERS_DIR, TEST_PROBLEM_DIR, TEST_RESULT_DIR, TympanTC,
+                   tybusiness, bus2solv)
+
+
+class TestTympan(TympanTC):
+    pass
+
+
+def make_test_with_file(test_file):
+    """ For a TEST_xx_NO_RESU.xml file from data/project-panel, load and
+    solve the corresponding project, load the expected result
+    from data/expected/TEST_xx.xml and compare the computed result with
+    the expected result.
+    The acoustic spectrums of the results are compared one by one (identified
+    by a receptor and source id), in a dB scale, with a 1 decimal definition.
+    """
+    def test_with_file(self):
+        # Load and solve the project
+        with self.no_output():
+            project = self.load_project(osp.join(TEST_PROBLEM_DIR, test_file))
+            computation = project.current_computation
+            computation.set_nthread(1) # avoid segfaults due to multithreading
+            bus2solv.loadsolver(TEST_SOLVERS_DIR, computation)
+            result = computation.go()
+        self.assertTrue(result)
+        # Load the expected result
+        result_file = osp.join(TEST_RESULT_DIR, test_file).replace('_NO_RESU', '')
+        with self.no_output():
+            expected_result_project = tybusiness.Project.from_xml(result_file)
+        # Compare results
+        current_result = computation.result
+        expected_result = expected_result_project.current_computation.result
+        # Check we have the same number of receptors
+        self.assertEqual(current_result.nreceptors, expected_result.nreceptors)
+        # Check if there is a control point (TYPointControl) among the receptors
+        # in the project.
+        # If not, we are not interested in checking the sources since the
+        # control points are the only receptors able to take into account the
+        # individual contributions of the sources.
+        # The sources here can be user sources, the machines and the buildings
+        # (TYUserSourcePonctuelle, TYMachine, TYBatiment)
+        check_nsources  = False
+        for i in xrange (current_result.nreceptors):
+            if current_result.receptor(i).is_control_point():
+                self.assertTrue(expected_result.receptor(i).is_control_point())
+                check_nsources = True
+        if check_nsources:
+            self.assertEqual(current_result.nsources, expected_result.nsources)
+        # check spectrums
+        for i in xrange(current_result.nreceptors):
+            for j in xrange(current_result.nsources):
+                curr_spectrum = current_result.spectrum(i, j)
+                expected_spectrum = expected_result.spectrum(i, j)
+                # Both spectrums must have the same number of elements
+                self.assertEqual(curr_spectrum.nvalues, expected_spectrum.nvalues)
+                # Let's compare the values in dB
+                curr_spectrum = curr_spectrum.to_dB()
+                expected_spectrum = expected_spectrum.to_dB()
+                np.testing.assert_almost_equal(curr_spectrum.values,
+                                               expected_spectrum.values, decimal=1)
+    return test_with_file
+
+
+# Retrieve all the available "TEST_XX" xml files and make a test for each one
+for test_file in os.listdir(TEST_PROBLEM_DIR):
+    if test_file.startswith('TEST_') and test_file.endswith('xml'):
+        setattr(TestTympan, "test_" + test_file.split('.')[0].replace('TEST_', '').lower(),
+                make_test_with_file(test_file))
+
+if __name__ == '__main__':
+    from utils import main
+    main()
---
+++ python/tympan/models/common.pxd
@@ -0,0 +1,80 @@
+"""shared models for Code_TYMPAN, headers
+"""
+
+from libcpp cimport bool
+from libcpp.string cimport string
+from libcpp.deque cimport deque
+
+cdef extern from "Tympan/models/common/3d.h":
+
+    cdef cppclass OMatrix:
+        pass
+    OPoint3D dot "operator*"(const OMatrix& mat, const OPoint3D& point)
+    # because "import operator*" triggers a syntax error
+
+    cdef cppclass OCoord3D:
+        double _x
+        double _y
+        double _z
+        double _value[3]
+        bool operator==(const OCoord3D& coord)
+
+    cdef cppclass OPoint3D (OCoord3D):
+        pass
+
+    cdef cppclass OVector3D (OCoord3D):
+        pass
+
+
+cdef extern from "Tympan/models/common/triangle.h":
+
+    cdef cppclass OTriangle:
+        OTriangle(int p1, int p2, int p3)
+        OPoint3D _A
+        OPoint3D _B
+        OPoint3D _C
+        int _p1
+        int _p2
+        int _p3
+        bool checkConsistencyWrtPointsTab(const deque[OPoint3D]& points) const
+        string reportInconsistencyWrtPointsTab(const deque[OPoint3D]& points)
+
+
+cdef extern from "Tympan/models/common/spectre.h":
+
+    const unsigned int TY_SPECTRE_DEFAULT_NB_ELMT
+
+    cdef enum TYSpectreType:
+        SPECTRE_TYPE_ATT
+        SPECTRE_TYPE_ABSO
+        SPECTRE_TYPE_LW
+        SPECTRE_TYPE_LP
+        SPECTRE_TYPE_AUTRE
+
+    cdef cppclass OSpectre:
+        OSpectre()
+        bool operator==(const OSpectre& other) const
+        bool operator!=(const OSpectre& other) const
+        OSpectre toDB()
+        void setType(TYSpectreType type)
+        double * getTabValReel()
+        unsigned int getNbValues() const
+
+
+cdef class Spectrum:
+    cdef OSpectre thisobj
+
+cdef class Point3D:
+    cdef OPoint3D thisobj
+
+cdef class Triangle:
+    # because OTriangle has no default constructor so we can't have an OTriangle
+    # allocated object as data member
+    cdef OTriangle* thisptr
+
+
+cdef ospectre2spectrum(OSpectre os)
+cdef opoint3d2point3d(OPoint3D pt)
+cdef otriangle2triangle(OTriangle* tri)
+
+
