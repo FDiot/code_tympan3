33dbe48b636bfd10f5121cf5dd319073a4d702cb
Anthony Truchet <anthony.truchet@logilab.fr>
2014-07-01 15:17:53 +0200

    [altimetry] Create datamodel bridged with shapely
    related to #1650179

---
+++ python/src/altimetry/datamodel.py
@@ -0,0 +1,244 @@
+"""
+Data model for the computation of the altimetry.
+
+The input classes implement the `geo_interface`_  protocol
+which enable them to be used directly with Shapely_ and allow
+to easily load test data from a GeoJSON_ file.
+
+.. _geo_interface: https://gist.github.com/sgillies/2217756
+.. _Shapely: http://toblerity.org/shapely/manual.html#python-geo-interface
+.. _GeoJSON: http://geojson.org/geojson-spec.html
+"""
+
+import json
+
+from shapely import geometry
+from shapely.validation import explain_validity
+
+
+GEOMETRY_TYPES = (
+    "Point", "MultiPoint",
+    "LineString", "MultiLineString",
+    "Polygon", "MultiPolygon",
+)
+
+
+def _preproc_one_coord(coord):
+    if len(coord) not in (2, 3):
+        raise ValueError("Coordinates are expected to be seq of 2 or 3 numbers")
+    return tuple(float(c) for c in coord)
+
+def _preproc_point_seq(coordinates):
+    return [_preproc_one_coord(c) for c in coordinates]
+
+
+
+class InvalidGeometry(Exception):
+    pass
+
+class GroundMaterial(object):
+
+    def __init__(self, id_):
+        self.id = id_
+
+
+MATERIAL_WATER = GroundMaterial("Water")
+
+
+class GeometricFeature(object):
+    geometric_type = None # To be overridden by derived classes
+
+    def __init__(self, coords, id):
+        self._coords = _preproc_point_seq(coords)
+        self.id = id
+        self._shape = None # to cache the Shapely shape
+
+    def build_coordinates(self):
+        raise NotImplementedError
+
+    def build_geometry(self):
+        if self.geometric_type not in GEOMETRY_TYPES:
+            return None
+        else:
+            d = {"type": self.geometric_type,
+                 "coordinates": self.build_coordinates()}
+            return d
+
+    def build_properties(self):
+        return {}
+
+    def build_feature(self):
+        d = {"type": "Feature",
+             "geometry" : self.build_geometry(),
+             "properties": self.build_properties()}
+        if self.id is not None:
+            d["id"] = self.id
+        return d
+
+    def build_shape(self):
+        return geometry.shape(self)
+
+    @property
+    def as_geojson(self):
+        return json.dumps(self.build_feature(), sort_keys=True)
+
+    def __repr__(self):
+        return self.as_geojson
+
+    @property
+    def __geo_interface__(self):
+        return self.build_geometry()
+
+    @property
+    def as_feature(self):
+        return self.build_feature()
+
+    @property
+    def as_shape(self):
+        return geometry.asShape(self)
+
+    @property
+    def shape(self):
+        if self._shape is None:
+            self._shape = self.build_shape()
+        return self._shape
+
+    def ensure_ok(self):
+        shape = self.shape # CAUTION Cache the shape
+        if shape.is_valid:
+            return
+        else :
+            raise InvalidGeometry(explain_validity(shape))
+
+
+class TympanFeature(GeometricFeature):
+
+    def __init__(self, coords, parent_site=None, **kwargs):
+        super(TympanFeature, self).__init__(coords, **kwargs)
+        self.parent_site = parent_site
+        if self.parent_site is not None:
+            assert isinstance(self.parent_site, (SiteNode,))
+            self.parent_site.add_child(self)
+
+    @property
+    def tympan_type(self):
+        return type(self).__name__
+
+    @property
+    def parent_site_id(self):
+        return  self.parent_site and self.parent_site.id
+
+    def build_properties(self):
+        p = super(TympanFeature, self).build_properties()
+        assert "type" not in p
+        p["type"] = self.tympan_type
+        if self.parent_site:
+            assert "site" not in p
+            p["site"] = self.parent_site_id
+        return p
+
+
+class LevelCurve(TympanFeature):
+    geometric_type = "LineString"
+
+    def __init__(self, coords, altitude, **kwargs):
+        try:
+            self.altitude = float(altitude)
+        except KeyError:
+            raise ValueError('LevelCurve(...) expect an `altitude` named argument')
+        super(LevelCurve, self).__init__(coords, **kwargs)
+
+    def build_coordinates(self):
+        return self._coords
+
+    def build_properties(self):
+        d = super(LevelCurve, self).build_properties()
+        d.update(altitude=self.altitude)
+        return d
+
+
+class PolygonalTympanFeature(TympanFeature):
+    geometric_type = "Polygon"
+
+    def __init__(self, coords, **kwargs):
+        self.holes = [_preproc_point_seq(hole) for hole in kwargs.pop("holes", [])]
+        super(PolygonalTympanFeature, self).__init__(coords, **kwargs)
+
+    def build_coordinates(self):
+        return [self._coords] + self.holes
+
+    def build_shape(self):
+        shape = super(PolygonalTympanFeature, self).build_shape()
+        if not shape.exterior.is_ccw:
+            shape = geometry.polygon.orient(shape)
+        return shape
+
+    def ensure_ok(self):
+        super(PolygonalTympanFeature, self).ensure_ok()
+        if not self.shape.exterior.is_simple:
+            raise InvalidGeometry("Polygon is expected to be simple")
+
+
+class MaterialArea(PolygonalTympanFeature):
+
+    def __init__(self, coords, material, **kwargs):
+        try:
+            self.material = material
+        except KeyError:
+            raise ValueError('Material(...) expect a `material` named argument')
+        super(MaterialArea, self).__init__(coords, **kwargs)
+
+    @property
+    def material_id(self):
+        return self.material.id
+
+    def build_properties(self):
+        d = super(MaterialArea, self).build_properties()
+        d.update(material=self.material.id)
+        return d
+
+
+
+class WaterBody(MaterialArea, LevelCurve):
+    geometric_type = "Polygon"
+
+    def __init__(self, coords, **kwargs):
+        super(WaterBody, self).__init__(coords, material=MATERIAL_WATER, **kwargs)
+
+
+class SiteNode(PolygonalTympanFeature):
+
+    CHILDREN_TYPES = ("LevelCurve", "MaterialArea", "WaterBody",
+                      "InfrastructureLandtake", "SiteNode")
+
+    def __init__(self, coords, **kwargs):
+        super(SiteNode, self).__init__(coords, **kwargs)
+
+        self.children = {}
+        for k in self.CHILDREN_TYPES:
+            self.children[k] = []
+
+    def add_child(self, child):
+        self.children[child.tympan_type].append(child)
+
+    @property
+    def level_curves(self):
+        return self.children["LevelCurve"] + self.children["WaterBody"]
+
+    @property
+    def material_areas(self):
+        return self.children["MaterialArea"] + self.children["WaterBody"]
+
+    @property
+    def subsites(self):
+        return self.children["SiteNode"][:]
+
+    @property
+    def landtakes(self):
+        return self.children["InfrastructureLandtake"][:]
+
+
+class InfrastructureLandtake(PolygonalTympanFeature):
+
+    def __init__(self, coords, **kwargs):
+        super(InfrastructureLandtake, self).__init__(coords, **kwargs)
--- python/tests/test_altimetry.py
+++ python/tests/test_altimetry.py
@@ -1,7 +1,118 @@
 import sys
 import os, os.path as osp
 
-from utils import TympanTC
+from shapely import geometry
+from shapely.geometry import Point, MultiLineString
+from altimetry.datamodel import * # NB also allows to find CGAL bindings
+
+
+class AltimetryDataTC(unittest.TestCase):
+
+    big_rect_coords = [(0, 0), (12,0), (12, 10), (0, 10)]
+
+    level_curve_A_coords = [(0, 0), (2.5, 3.5), (4, 4.0)]
+    level_curve_A =  LevelCurve(level_curve_A_coords, altitude=10.0, id="{123456}")
+    level_curve_A_json = (
+        '{"geometry": {"coordinates": [[0.0, 0.0], [2.5, 3.5], [4.0, 4.0]],'
+        ' "type": "LineString"}, "id": "{123456}",'
+        ' "properties": {"altitude": 10.0, "type": "LevelCurve"},'
+        ' "type": "Feature"}')
+    material_area_A_coords = big_rect_coords
+    grass = GroundMaterial("grass")
+    material_area_A = MaterialArea(material_area_A_coords, material=grass, id=None)
+    material_area_A_json = (
+        '{"geometry": {'
+        '"coordinates": [[[0.0, 0.0], [12.0, 0.0], [12.0, 10.0], [0.0, 10.0]]],'
+        ' "type": "Polygon"},'
+        ' "properties": {"material": "grass", "type": "MaterialArea"},'
+        ' "type": "Feature"}')
+    waterbody_coords = [(3, 3), (5, 4), (3, 5)]
+
+    subsite_A_coords = [(8.0, 4.0), (8.0, 7.0), (12.0, 7.0)]
+    subsite_A = SiteNode(subsite_A_coords, id='{Sub-site A ID}')
+    level_curve_B_coords =[(8.0, 4.0), (8.0, 7.0), (12.0, 7.0)]
+
+
+    def test_LevelCurve_as_json(self):
+        self.assertEqual(self.level_curve_A.as_geojson, self.level_curve_A_json)
+
+    def test_MaterialArea_as_json(self):
+        self.assertEqual(self.material_area_A.as_geojson, self.material_area_A_json)
+
+    def test_LevelCurve_as_shape(self):
+        shape = self.level_curve_A.as_shape
+        self.assertTrue(shape.equals(geometry.LineString(self.level_curve_A_coords)))
+
+    def test_MaterialArea_as_shape(self):
+        shape = geometry.shape(self.material_area_A)
+        self.assertTrue(shape.equals(geometry.Polygon(self.material_area_A_coords)))
+
+    def test_SiteNode_as_shape(self):
+        shape = geometry.shape(self.subsite_A)
+        self.assertTrue(shape.equals(geometry.Polygon(self.subsite_A_coords)))
+
+    def test_site_belonging(self):
+        self.assertIsNone(self.subsite_A.parent_site)
+        level_curve_B =  LevelCurve(self.level_curve_B_coords, altitude=20.0,
+                                    parent_site=self.subsite_A, id=None)
+        self.assertIn(level_curve_B, self.subsite_A.children["LevelCurve"])
+        self.assertEqual(level_curve_B.parent_site_id, "{Sub-site A ID}")
+
+
+    def test_cached_shape_property(self):
+        level_curve_B =  LevelCurve(self.level_curve_B_coords, altitude=20.0, id=None)
+        self.assertIsNone(level_curve_B._shape)
+
+        shape = level_curve_B.shape
+        self.assertTrue(shape.equals(level_curve_B.as_shape))
+        self.assertIs(shape, level_curve_B._shape)
+
+    def test_polygon_validity(self):
+        poly = PolygonalTympanFeature(self.big_rect_coords, id=None)
+        try:
+            poly.ensure_ok()
+        except InvalidGeometry:
+            self.fail("poly was expected to be valid")
+
+        coords = self.big_rect_coords[:]
+        coords[1:3] = coords[2:0:-1]
+        poly2 = PolygonalTympanFeature(coords, id=None)
+
+        with self.assertRaises(InvalidGeometry) as cm:
+            poly2.ensure_ok()
+        self.assertIn("Self-intersection", cm.exception.args[0])
+
+
+    def test_polygon_exterior_orientation(self):
+        coords = self.big_rect_coords[:]
+        coords.reverse() # change orientation to CW
+        poly = PolygonalTympanFeature(coords, id=None)
+
+        self.assertTrue(poly.shape.exterior.is_ccw)
+
+    def test_WaterBody_initialisation(self):
+        waterbody=  WaterBody(self.waterbody_coords, altitude=20, id=None)
+
+        self.assertEqual(waterbody.altitude, 20.0)
+        self.assertEqual(waterbody.material, MATERIAL_WATER)
+
+    def test_sitenode_attribution_and_accessors(self):
+        mainsite = SiteNode(self.big_rect_coords, id="{Main site ID}")
+        subsite = SiteNode(self.subsite_A_coords, id="{Subsite ID}",
+                           parent_site=mainsite)
+        level_curve_B =  LevelCurve(self.level_curve_B_coords, altitude=20.0,
+                                    parent_site=subsite, id=None)
+        level_curve_A =  LevelCurve(self.level_curve_A_coords, altitude=10.0,
+                                    parent_site=mainsite, id=None)
+        waterbody=  WaterBody(self.waterbody_coords, altitude=5,
+                              parent_site=mainsite, id=None)
+        material_area_A = MaterialArea(self.material_area_A_coords, material=self.grass,
+                                       parent_site=mainsite, id=None)
+
+        self.assertItemsEqual(mainsite.level_curves, [level_curve_A, waterbody])
+        self.assertItemsEqual(mainsite.material_areas, [material_area_A, waterbody])
+        self.assertItemsEqual(mainsite.subsites, [subsite])
+        self.assertItemsEqual(subsite.level_curves, [level_curve_B])
 
 class TestAltimetry(TympanTC):
     def test_environment(self):
