eea45535b598a0c83bdd451c9c8cb45af3d085cf
Sylvain Th?nault <sylvain.thenault@logilab.fr>
2014-07-03 08:53:10 +0200

    [refactor]?extract function to start python process from the calcul manager
    and put it, along with related private utilities functions into
    Tympan/gui/app/os module

--- Tympan/gui/app/TYCalculManager.cpp
+++ Tympan/gui/app/TYCalculManager.cpp
@@ -19,7 +19,6 @@
  * pour la partie calcul
  */
 
-
 #include <qcursor.h>
 #include <qmessagebox.h>
 #include <QTemporaryFile>
@@ -29,8 +28,8 @@
 #include "Tympan/core/defines.h"
 #include "Tympan/models/business/OLocalizator.h"
 #include "Tympan/models/business/xml_project_util.h"
+#include "Tympan/gui/app/os.h"
 #include "Tympan/gui/app/TYApplication.h"
-#include "Tympan/gui/app/EnvironmentUtils.h"
 #include "Tympan/gui/app/TYProjetFrame.h"
 #include "Tympan/gui/app/TYMessageManager.h"
 #include "Tympan/gui/app/TYMainWindow.h"
@@ -38,7 +37,6 @@
 
 
 #define TR(id) OLocalizator::getString("TYCalculManager", (id))
-#define COMPUTATION_TIMEOUT 10000 // In ms
 
 using namespace tympan;
 
@@ -85,22 +83,21 @@ bool TYCalculManager::launch(LPTYCalcul pCalcul)
             return false;
         }
         problemfile.close();
-
         // Serialize current project
-        ostringstream msg;
         try
         {
             save_project(problemfile.fileName().toUtf8().data(), pProject);
         }
         catch(const tympan::invalid_data& exc)
         {
+            ostringstream msg;
             msg << boost::diagnostic_information(exc);
             logger.error(
                     "Could not export current project. Computation won't be done");
             logger.debug(msg.str().c_str());
             return false;
         }
-
+        // Initialize result file
         QTemporaryFile resultfile;
         if (!resultfile.open())
         {
@@ -115,7 +112,7 @@ bool TYCalculManager::launch(LPTYCalcul pCalcul)
         if (tympan_debug_idx >= 0)
         {
             QString debug_option = appli_env[tympan_debug_idx].split('=')[1];
-            if (debug_option.contains("xml", Qt::CaseInsensitive))// == "xml")
+            if (debug_option.contains("xml", Qt::CaseInsensitive))
             {
                 // If we are in debug mode, don't remove the temporary files once python
                 // script is done
@@ -132,7 +129,6 @@ bool TYCalculManager::launch(LPTYCalcul pCalcul)
         // containing the problem, the name of the file where to record
         // the result and the directory containing the solver plugin to use
         // to solve the acoustic problem
-        QProcess python;
         QStringList args;
         QString absolute_plugins_path (QCoreApplication::applicationDirPath());
         absolute_plugins_path.append("/");
@@ -145,75 +141,8 @@ bool TYCalculManager::launch(LPTYCalcul pCalcul)
 
         logger.info(TR("id_msg_go_calcul"));
 
-        // Deactivate GUI
-        getTYMainWnd()->setEnabled(false);
-        TYApplication::setOverrideCursor(QCursor(Qt::WaitCursor));
-
-        float comp_duration (0.);
-        bool comp_finished (false);
-        // Send python script output to the current process std::out/err
-        python.setProcessChannelMode(QProcess::ForwardedChannels);
-        // Set PYTHONPATH to python subprocess
-        QStringList env(python_qprocess_environment());
-        python.setEnvironment(env);
-        // Since scripts passed to QProcess are not launched through cmd.exe
-        // under windows, we have to give QProcess the path to the python interpreter
-        QString python_interp;
-        try
+        if (!python(args))
         {
-            python_interp = get_python_interp();
-        }
-        catch(const tympan::invalid_data& exc)
-        {
-            msg << boost::diagnostic_information(exc);
-            logger.error(
-                    "Could not find python interpreter to launch python script. Computation won't be done");
-            logger.debug(msg.str().c_str());
-            // Reactivate GUI
-            TYApplication::restoreOverrideCursor();
-            getTYMainWnd()->setEnabled(true);
-            QMessageBox msgBox;
-            msgBox.setText(
-                    "L'interpreteur python n'a pas pu etre trouve.\nVeuillez verifier que la variable d'environnement TYMPAN_PYTHON_INTERP est correctement positionnee");
-            msgBox.exec();
-            return false;
-        }
-        python.start(python_interp, args);
-        do
-        {
-            comp_finished = python.waitForFinished(COMPUTATION_TIMEOUT);
-            if (comp_finished)
-            {
-                break;
-            }
-            if (python.error() == QProcess::Timedout)
-            {
-                //Computation still running
-                comp_duration += (COMPUTATION_TIMEOUT/1000);
-                logger.info("Computation still running after %.3f seconds", comp_duration);
-                continue;
-            }
-            else
-            {
-                // Will have a bad exit status and will be handled below
-                break;
-            }
-        }
-        while(!comp_finished);
-
-        int pystatus = python.exitStatus();
-        if (pystatus == 1)
-        {
-            logger.error(
-                    "QProcess running python script had a bad exit status. Error: %d. No results available.",
-                    python.error());
-            logger.info(TR("id_msg_calcul_failed"));
-            // Reactivate GUI
-            TYApplication::restoreOverrideCursor();
-            getTYMainWnd()->setEnabled(true);
-            QMessageBox msgBox;
-            msgBox.setText("Le calcul a echoue, veuillez reessayer.");
-            msgBox.exec();
             return false;
         }
         // Then read the result to update the internal model
@@ -224,18 +153,15 @@ bool TYCalculManager::launch(LPTYCalcul pCalcul)
         }
         catch(const tympan::invalid_data& exc)
         {
+            ostringstream msg;
             msg << boost::diagnostic_information(exc);
             logger.error("Could not import computed project. No results available.");
             logger.debug(msg.str().c_str());
-            // reactivate GUI
-            TYApplication::restoreOverrideCursor();
-            getTYMainWnd()->setEnabled(true);
             QMessageBox msgBox;
             msgBox.setText("Le fichier de resultats n'a pas pu etre lu.");
             msgBox.exec();
             return false;
         }
-
         // Update the current project with the results of the current acoustic
         // problem
         TYCalcul* pOldComp = pCalcul;
@@ -247,30 +173,15 @@ bool TYCalculManager::launch(LPTYCalcul pCalcul)
         getTYApp()->getCurProjet()->remCalcul(pOldComp);
         getTYMainWnd()->getProjetFrame()->setProjet(pProject);
 
-    // Compute and display computation time
-    OChronoTime endTime;
-    OChronoTime duration = endTime - startTime;
-    unsigned long second  = duration.getTime() / 1000;
-    unsigned long millisecond = duration.getTime() - second * 1000;
-    logger.info("Temps de calcul : %02ld,%03ld sec.  (%ld msec.)", second,
-            millisecond, duration.getTime());
-
-    // Reactivate GUI
-    TYApplication::restoreOverrideCursor();
-    getTYMainWnd()->setEnabled(true);
-
     // Update graphics
     pCalcul->getParent()->updateGraphicTree();
     pCalcul->updateGraphicTree();
     getTYMainWnd()->updateModelers(false, false);
     TYElement::setIsSavedOk(true);
-
     // Update projet frame
     getTYMainWnd()->getProjetFrame()->setCalculDone(true);
-
     // Computation achieved with success
     logger.info(TR("id_msg_calcul_done"));
-
     return true;
 }
 
---
+++ Tympan/gui/app/os.h
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) <2014> <EDF-R&D> <FRANCE>
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+/**
+ * \file Tympan/gui/app/os.h
+ * \brief Utilitaires pour les interactions entre l'interface graphique et le système (headers)
+ */
+
+
+#ifndef TY_G_A_OS
+#define TY_G_A_OS
+
+#include <qstring.h>
+
+bool python(QStringList args);
+
+#endif // TY_G_A_OS
