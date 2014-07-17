bd084439cfd714fae766128deee43af5513e3c9a
Laura M?dioni <laura.medioni@logilab.fr>
2014-07-17 16:39:20 +0200

    [cmake] Add CGAL_BINDINGS_PATH environment variable to the altimetry test env
    Related to #1961496

---
+++ python/tests/test_altimetry.py
@@ -0,0 +1,27 @@
+import sys
+import os, os.path as osp
+
+from utils import TympanTC
+
+class TestAltimetry(TympanTC):
+    def test_environment(self):
+        # Import shapely (must be in the PYTHONPATH)
+        try:
+            import shapely
+        except ImportError:
+            self.fail('Shapely must be installed and must be in your PYTHONPATH')
+        # import CGAL bindings (CGAL_BINDINGS_PATH environment variable must be defined)
+        CGAL_BINDINGS_PATH = os.environ.get('CGAL_BINDINGS_PATH')
+        self.assertTrue(CGAL_BINDINGS_PATH is not None)
+        self.assertTrue(osp.isdir(CGAL_BINDINGS_PATH))
+        sys.path.append(CGAL_BINDINGS_PATH)
+        try:
+            import CGAL
+            from CGAL import CGAL_Kernel, CGAL_Mesh_2
+        except ImportError:
+            self.fail('''CGAL_BINDINGS_PATH environment variable must contain
+                      the path to CGAL_bindings python libraries''')
+
+if __name__ == '__main__':
+    from utils import main
+    main()
