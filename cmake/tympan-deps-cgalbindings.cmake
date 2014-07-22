670982b701f5a68f1138b747e43dfd5dac664960
Anthony Truchet <anthony.truchet@logilab.fr>
2014-07-22 18:53:19 +0200

    [cmake] fix installation of cgal bindings

--- cmake/tympan-deps-cgalbindings.cmake
+++ cmake/tympan-deps-cgalbindings.cmake
@@ -2,33 +2,31 @@
 ExternalProject_Add(CGAL_bindings
   URL "${TYMPAN_3RDPARTY_CGAL_BINDINGS}"
   URL_MD5 "${TYMPAN_3RDPARTY_CGAL_BINDINGS_MD5}"
-  CMAKE_ARGS -DBUILD_JAVA=OFF
+  CMAKE_ARGS -DBUILD_JAVA=OFF -DCMAKE_BUILD_TYPE:STRING=Release -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR>
   BUILD_COMMAND ${CMAKE_COMMAND} --build . --config Release
-  INSTALL_COMMAND ""
+  INSTALL_COMMAND ${CMAKE_COMMAND} --build . --config Release --target install
   BUILD_IN_SOURCE 0
 )
 # Get BINARY_DIR property from CGAL_bindings project.
+ExternalProject_Get_Property(CGAL_bindings INSTALL_DIR)
+set(CGAL_BINDINGS_PYTHON_DIR ${INSTALL_DIR}/cgal-bindings/CGAL)
 ExternalProject_Get_Property(CGAL_bindings BINARY_DIR)
-set(CGAL_BINDINGS_DIR ${BINARY_DIR}/build-python)
-set(CGAL_BINDINGS_LIB_DIR ${CGAL_BINDINGS_DIR}/CGAL)
+set(CGAL_BINDINGS_LIB_DIR ${BINARY_DIR}/lib)
 
-link_directories(${CGAL_BINDINGS_LIB_DIR})
-
+# Install language independent libraries from CGAL bindings
 if(SYS_NATIVE_WIN)
-    install(FILES ${CGAL_BINDINGS_LIB_DIR}/CGAL_HalfedgeDS.dll
-      ${CGAL_BINDINGS_LIB_DIR}/CGAL_Kernel.dll
-      ${CGAL_BINDINGS_LIB_DIR}/CGAL_Mesh_2.dll
-      ${CGAL_BINDINGS_LIB_DIR}/CGAL_Triangulation_2_cpp.dll
-    DESTINATION .)
-list(APPEND TYMPAN_3RDPARTY_DLL_DIRS ${CGAL_BINDINGS_LIB_DIR})
+install(DIRECTORY ${CGAL_BINDINGS_LIB_DIR}/Release/ #Significant trailing slash!
+    DESTINATION .
+    FILES_MATCHING PATTERN "*.dll")
 else()
-    set(filename _CGAL_HalfedgeDS${CMAKE_SHARED_LIBRARY_SUFFIX})
-    install(FILES ${CGAL_BINDINGS_LIB_DIR}/${filename} DESTINATION lib)
-    set(filename _CGAL_Kernel${CMAKE_SHARED_LIBRARY_SUFFIX})
-    install(FILES ${CGAL_BINDINGS_LIB_DIR}/${filename} DESTINATION lib)
-    set(filename _CGAL_Mesh_2${CMAKE_SHARED_LIBRARY_SUFFIX})
-    install(FILES ${CGAL_BINDINGS_LIB_DIR}/${filename} DESTINATION lib)
-    set(filename _CGAL_Triangulation_2${CMAKE_SHARED_LIBRARY_SUFFIX})
-    install(FILES ${CGAL_BINDINGS_LIB_DIR}/${filename} DESTINATION lib)
-    set(filename)
+  install(DIRECTORY ${CGAL_BINDINGS_LIB_DIR}/ #Significant trailing slash!
+    DESTINATION "lib"
+    FILES_MATCHING PATTERN "*.so")
 endif(SYS_NATIVE_WIN)
+# Install Python modules 
+install(DIRECTORY ${CGAL_BINDINGS_PYTHON_DIR} #Significant NO trailing slash!
+    DESTINATION "${TYMPAN_CythonModules_Release}"
+    CONFIGURATIONS Release)
+install(DIRECTORY ${CGAL_BINDINGS_PYTHON_DIR} #Significant NO trailing slash!
+    DESTINATION "${TYMPAN_CythonModules_Debug}"
+    CONFIGURATIONS Debug)
