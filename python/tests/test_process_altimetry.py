af3cff7b2fb1789edfb7ace70416844c392636da
Laura M?dioni <laura.medioni@logilab.fr>
2014-07-24 16:54:39 +0200

    [cython] add water body creation in process_altimetry
    Related to #1961496

--- python/tests/test_process_altimetry.py
+++ python/tests/test_process_altimetry.py
@@ -42,6 +42,22 @@ class TestProcessAltimetry(TympanTC):
                          [(-197.0, 197.0), (197.0, 197.0), (197.0, -195.0),
                           (-196.0, -197.0), (-197.0, 197.0)])
 
+    def test_process_water_bodies(self):
+        with self.no_output():
+            asite = tyalti.process_site_altimetry(
+                osp.join(TEST_PROBLEM_DIR, '7_PROJET_Site_emprise_seule_avec_plan_eau.xml'))
+        lakes = asite.material_areas # water bodies are treated as material areas
+        self.assertEqual(len(lakes), 1) # just 1 water body, no other material areas
+        lake = lakes[0]
+        self.assertEqual(lake.build_coordinates()[0],
+                         [(-83.0, -104.0), (-137.0, -80.0), (-141.0, -45.0),
+                          (-117.0, 7.0), (-90.0, 22.0), (-49.0, 21.0),
+                          (-19.0, 24.0), (-2.0, 64.0), (54.0, 95.0),
+                          (109.0, 66.0), (122.0, -4.0), (75.0, -48.0),
+                          (26.0, -62.0), (3.0, -115.0), (-33.0, -153.0),
+                          (-75.0, -136.0)])
+        self.assertEqual(lake.id, "{1d99f26d-83df-4404-94a4-7e3c5eccce13}")
+
 if __name__ == '__main__':
     from utils import main, config_cython_extensions_path
     config_cython_extensions_path()
