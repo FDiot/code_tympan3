4e678c2f4cda5afa4551c6dd18a319240184568c
Anthony Truchet <anthony.truchet@logilab.fr>
2014-07-24 18:03:32 +0200

    [altimetry] Setup use of the PLY file format
    Configure and install two minor dependencies for reading and writting PLY files
    PLY files are simple file for storing polygonal meshes.
    
    The PLY file format is documented here :
        http://www.cs.virginia.edu/~gfx/Courses/2001/Advanced.spring.01/plylib/Ply.txt
    The following C library is used from the C++ side:
        http://w3.impa.br/~diego/software/rply/
    The following Python module is used from the Python side:
        https://github.com/dranjan/python-plyfile

---
+++ cmake/tympan-deps-ply.cmake
@@ -0,0 +1,31 @@
+# Configure and install two minor dependencies for reading and writting PLY files
+# PLY files are simple file for storing polygonal meshes.
+#
+# The PLY file format is documented here :
+#     http://www.cs.virginia.edu/~gfx/Courses/2001/Advanced.spring.01/plylib/Ply.txt
+# The following C library is used from the C++ side:
+#     http://w3.impa.br/~diego/software/rply/
+# The following Python module is used from the Python side:
+#     https://github.com/dranjan/python-plyfile
+
+set(RPLY_DIR "${CMAKE_SOURCE_DIR}/3rdparty/rply-1.1.3"
+  CACHE PATH "The root directory for the RPLY source tree")
+set(RPLY_SOURCES "${RPLY_DIR}/rply.c")
+set(RPLY_INCLUDE_DIRECTORY "${RPLY_DIR}")
+add_library(rply STATIC ${RPLY_SOURCES})
+set(RPLY_LIB rply)
+
+set(PYTHONPLYFILE_DIR "${CMAKE_SOURCE_DIR}/3rdparty/python-plyfile"
+  CACHE PATH "The root directory for the plyfile python source tree")
+set(PYTHONPLYFILE_SOURCES "${PYTHONPLYFILE_DIR}/plyfile.py")
+file(COPY ${PYTHONPLYFILE_SOURCES} 
+  DESTINATION "${PROJECT_BINARY_DIR}/${TYMPAN_CythonModules_Debug}")
+file(COPY ${PYTHONPLYFILE_SOURCES} 
+  DESTINATION "${PROJECT_BINARY_DIR}/${TYMPAN_CythonModules_Release}")
+install(FILES ${PYTHONPLYFILE_SOURCES} 
+  DESTINATION "${TYMPAN_CythonModules_Release}/"
+  CONFIGURATIONS Release)
+install(FILES ${PYTHONPLYFILE_SOURCES} 
+  DESTINATION "${TYMPAN_CythonModules_Debug}/"
+  CONFIGURATIONS Debug)
+
---
+++ python/tests/test_plyfile.py
@@ -0,0 +1,41 @@
+import os
+import numpy  as np
+import unittest
+import tempfile
+
+from utils import main
+
+DEBUG_TMP_FILES = os.environ.get('DEBUG_TMP_FILES', False)
+
+class PLYFileTC(unittest.TestCase):
+    def test_can_import(self):
+        try:
+            import plyfile
+        except ImportError:
+            self.fail("Can not import plyfile module : check its installation in the build tree")
+
+    def build_ply_single_triangle(self):
+        from plyfile import PlyElement, PlyData
+        points_dt = np.dtype([('x', float), ('y', float), ('z', float)])
+        face_dt = np.dtype([('vertex_indices', np.uint32, (3,))])
+        # The numpy array data.
+        points = np.array([(-1,0,0), (0,1,0), (1,0,0)], dtype=points_dt)
+        triangles = np.array([((1, 0, 2),),], dtype=face_dt) #The Tuple with a tuple inside is required Why !?!
+        elements = [PlyElement.describe(points, "vertex"),
+                    PlyElement.describe(triangles, "face",
+                                        len_types={'vertex_indices': 'u1'},
+                                        val_types={'vertex_indices': 'u4'}),]
+        ply_data = PlyData(elements, text=True)
+        return ply_data
+
+    def test_write_trivial_file(self):
+        ply_data = self.build_ply_single_triangle()
+
+        with tempfile.NamedTemporaryFile(suffix='.ply', delete=(not DEBUG_TMP_FILES)) as f:
+            ply_data.write(f)
+            if DEBUG_TMP_FILES:
+                print "Temp. file for test_write_trivial_file: ", f.name
+
+
+if __name__ == '__main__':
+    main()
