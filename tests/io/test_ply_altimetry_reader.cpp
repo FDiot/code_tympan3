4662f15de7b73c5ffa00476eb6fe56a98ec3e3e4
Anthony Truchet <anthony.truchet@logilab.fr>
2014-07-30 15:21:30 +0200

    [altimetry] Fetch RPly information in C callbacks

--- tests/io/test_ply_altimetry_reader.cpp
+++ tests/io/test_ply_altimetry_reader.cpp
@@ -14,4 +14,17 @@ TEST(TestAltimetryReader, instanciation)
 
     std::unique_ptr<IMeshReader> p_reader = make_altimetry_ply_reader(alti_file);
     EXPECT_TRUE(p_reader != nullptr);
+
+    ASSERT_THROW(make_altimetry_ply_reader("nonexistent.ply"),
+                 tympan::mesh_io_error);
+}
+
+
+TEST(TestAltimetryReader, trivial_read)
+{
+    // PLY files to read.
+    std::string alti_file = tympan::path_to_test_data("trivial.ply");
+
+    std::unique_ptr<IMeshReader> p_reader = make_altimetry_ply_reader(alti_file);
+    ASSERT_TRUE(p_reader != nullptr);
 }
