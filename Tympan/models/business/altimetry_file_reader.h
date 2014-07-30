34f4b426c0fac44c5c56cdeca1d118f01bd3393c
Anthony Truchet <anthony.truchet@logilab.fr>
2014-07-30 14:44:32 +0200

    [altimetry] create the a reader from PLY files for altimetry

---
+++ Tympan/models/business/altimetry_file_reader.h
@@ -0,0 +1,52 @@
+/**
+ * @file altimetry_file_reader.h
+ *
+ * @brief Provides for reading back an altimetry written in a file
+ *
+ * @author Anthony Truchet <anthony.truchet@logilab.fr>
+ *
+ */
+
+#ifndef TYMPAN__ALTIMETRYREADER_HPP__INCLUDED
+#define TYMPAN__ALTIMETRYREADER_HPP__INCLUDED
+
+#include <memory>
+#include <deque>
+#include <string>
+
+#include "Tympan/models/common/3d.h"
+#include "Tympan/models/common/triangle.h"
+
+namespace tympan {
+
+    class IMeshReader
+    {
+    public:
+        /// @brief Type of the points array returned
+        typedef std::deque<OPoint3D> points_array_t;
+        /// @brief Type of the faces array returned
+        typedef std::deque<OTriangle> faces_array_t;
+
+        /**
+         * @brief Getter for the points read
+         */
+        virtual const points_array_t& points() = 0;
+
+        /**
+         * @brief Getter for the faces read
+         */
+        virtual const faces_array_t& faces() = 0;
+
+        /**
+         * @brief read the file whose name was given at reader's construction time
+         */
+        virtual void read() = 0;
+    };
+
+    /// @ Makes a reader for the file name given as argument
+    std::unique_ptr<IMeshReader>
+    make_altimetry_ply_reader(const std::string filename);
+
+}; // namespace tympan
+
+#endif // TYMPAN__ALTIMETRYREADER_HPP__INCLUDED
