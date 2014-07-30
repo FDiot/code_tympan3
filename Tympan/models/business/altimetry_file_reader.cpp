3adbae3f439ba3d1925ff4b916aa0be0d3e70bdf
Anthony Truchet <anthony.truchet@logilab.fr>
2014-07-30 16:51:10 +0200

    [cmake, altimetry] AltimetryFileReader wrap a ply_t object form RPLY
    NB: Build RPLY as a shared object / DLL to enable its linking with tympan_lib

--- Tympan/models/business/altimetry_file_reader.cpp
+++ Tympan/models/business/altimetry_file_reader.cpp
@@ -7,9 +7,19 @@
  *
  */
 
+#include <boost/exception/errinfo_file_name.hpp>
+#include <boost/exception/errinfo_at_line.hpp>
+#include <boost/exception/errinfo_errno.hpp>
+
+#include "rply.h"
+
 #include "altimetry_file_reader.h"
 
-#include "Tympan/core/exceptions.h"
+// section with C linkage for RPLY callbacks
+extern "C" {
+    static int vertex_cb(p_ply_argument argument){return 1;};
+    static int face_cb(p_ply_argument argument){return 1;};
+} // extern C
 
 namespace tympan {
 
@@ -20,8 +30,8 @@ namespace tympan {
         public IMeshReader
     {
     public:
-        AltimetryPLYReader(const std::string filename) :
-            _filename(filename) {}
+        AltimetryPLYReader(const std::string filename);
+        virtual ~AltimetryPLYReader();
 
         /// Implements interface \c IMeshReader
         /// @{
@@ -30,11 +40,15 @@ namespace tympan {
         virtual void read();
         /// @}
 
-    protected:
+    private: // Data attributes
         points_array_t _points;
         faces_array_t _faces;
         std::string _filename;
-    }; // class AltimetryPLYReader
+        p_ply _ply;
+
+    }; // AltimetryPLYReader
+
+    //========== Implementation of free functions ==========
 
     std::unique_ptr<IMeshReader>
     make_altimetry_ply_reader(const std::string filename)
@@ -42,10 +56,32 @@ namespace tympan {
         return std::unique_ptr<IMeshReader>(new AltimetryPLYReader(filename));
     } //make_altimetry_ply_reader
 
+    //========== Implementation of AltimetryPLYReader methods ==========
+
+    AltimetryPLYReader::AltimetryPLYReader(const std::string filename) :
+        _filename(filename)
+    {
+        // Opens the PLY file, no error callback is given
+        _ply = ply_open(_filename.c_str(), NULL, 0, NULL);
+        if(_ply==NULL)
+            throw mesh_io_error("Opening PLY file")
+                << tympan_source_loc
+                << boost::errinfo_file_name(filename);
+        // Reads the header
+        if(!ply_read_header(_ply))
+            throw mesh_io_error("Reading the PLY header")
+                << tympan_source_loc
+                << boost::errinfo_file_name(filename);
+    }
+
+    AltimetryPLYReader::~AltimetryPLYReader()
+    {
+        ply_close(_ply);
+    }
 
     void AltimetryPLYReader::read()
     {
         // XXX TODO
-    } // AltimetryPLYReader::read()
+    }
 
 }; // namespace tympan
