84202f206c79191f159a856c79a7b2e51436e480
Anthony Truchet <anthony.truchet@logilab.fr>
2014-07-31 12:12:38 +0200

    [paltimetry] Implement vertex_cb

--- tests/io/test_ply_altimetry_reader.cpp
+++ tests/io/test_ply_altimetry_reader.cpp
@@ -34,3 +34,23 @@ TEST(TestAltimetryReader, trivial_setup_callbacks)
     EXPECT_EQ(1, reader.nfaces());
     EXPECT_GT(1, reader.faces().capacity());
 }
+
+TEST(TestAltimetryReader, vertex_cb)
+{
+    // PLY files to read.
+    std::string alti_file = tympan::path_to_test_data("trivial.ply");
+    AltimetryPLYReader reader(alti_file);
+    const OPoint3D p(1.0, 2.0, 3.0);
+    ASSERT_EQ(0, reader.points().size());
+    bool ok;
+
+    ok = reader.vertex_cb(AltimetryPLYReader::X, 0, p._x);
+    ASSERT_TRUE(ok);
+    EXPECT_EQ(1, reader.points().size());
+
+    ok =  reader.vertex_cb(AltimetryPLYReader::Y, 0, p._y);
+    ok &= reader.vertex_cb(AltimetryPLYReader::Z, 0, p._z);
+    ASSERT_TRUE(ok);
+    EXPECT_EQ(1, reader.points().size());
+    EXPECT_EQ(p, reader.points().back());
+}
