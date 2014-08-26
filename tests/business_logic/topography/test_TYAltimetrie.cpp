5da86dc26a252dd9eee944a9f2325dbd4c3514e0
Laura M?dioni <laura.medioni@logilab.fr>
2014-08-26 17:45:05 +0200

    [tests] adapt test_TYAltimetrie to use the new python altimetry
    related to #2741286

--- tests/business_logic/topography/test_TYAltimetrie.cpp
+++ tests/business_logic/topography/test_TYAltimetrie.cpp
@@ -18,15 +18,40 @@
 #include "Tympan/models/business/infrastructure/TYTopographie.h"
 #include "Tympan/models/business/infrastructure/TYSiteNode.h"
 #include "Tympan/models/business/material/TYSol.h"
+#include "Tympan/models/business/altimetry_file_reader_impl.h"
+#include "Tympan/models/business/subprocess_util.h"
+#include "Tympan/models/business/xml_project_util.h"
+#include "TympanTestsConfig.hpp"
 
-TYAltimetrie* buildSlopeAltimetry(void)
+static  const double precision = 1e-6;
+
+void update_altimetry(LPTYSiteNode pSite)
 {
-    // Creating the topography
-    TYTopographie* pTopo = new TYTopographie();
-    TYAltimetrie* pAlti = new TYAltimetrie();
+    QTemporaryFile xml_project;
+    QTemporaryFile mesh;
+    init_tmp_file(xml_project, false);
+    init_tmp_file(mesh, false);
+    tympan::save_project(xml_project.fileName().toUtf8().data(), pSite->getProjet());
+    QStringList args;
+    args << QString(tympan::path_to_python_scripts("process_site_altimetry.py").c_str()) << xml_project.fileName()
+        << mesh.fileName();
+    string error_msg;
+    python(args, error_msg);
+    std::deque<OPoint3D> points;
+    std::deque<OTriangle> triangles;
+    std::deque<LPTYSol> materials;
+    pSite->readMesh(points, triangles, materials, mesh.fileName());
+    pSite->getTopographie()->getAltimetrie()->plugBackTriangulation(points, triangles, materials);
+}
+
+
+LPTYSiteNode buildSiteSlopeAltimetry(void)
+{
+    LPTYSiteNode pSite = new TYSiteNode();
+    LPTYTopographie pTopo = pSite->getTopographie();
     TYCourbeNiveau* pCrb = NULL;
     // Those dimension match with the default emprise
-    const double xMin = -200.0, xMax = 200.0, yMin = -200.0, yMax = +200.0;
+    const double xMin = -180.0, xMax = 180.0, yMin = -180.0, yMax = +180.0;
     double z;
     for (double x = xMin; x <= xMax; x += 10.)
     {
@@ -38,48 +63,28 @@ TYAltimetrie* buildSlopeAltimetry(void)
         pCrb->addPoint(x, yMax, z);
         pCrb->setAltitude(z);
         pCrb->setDistMax(10.0);
-
         // Adding the level curve to the topography
         pTopo->addCrbNiv(pCrb);
     }
-
-    // Creating the altimetry
-    std::deque<OPoint3D> points;
-    std::deque<OTriangle> triangles;
-    std::deque<LPTYSol> materials;
-    LPTYSol stub_ground_mat;
-    // XXX  Stub:
-    points.push_back(OPoint3D(-200.0, 200.0, 0.0));
-    points.push_back(OPoint3D(200.0, 200.0, 0.0));
-    points.push_back(OPoint3D(200.0, -200.0, 0.0));
-    points.push_back(OPoint3D(-200.0, -200.0, 0.0));
-    triangles.push_back(OTriangle(1, 0, 2));
-    triangles.push_back(OTriangle(0, 3, 2));
-    materials.push_back(stub_ground_mat);
-    materials.push_back(stub_ground_mat);
-    pAlti->plugBackTriangulation(points, triangles, materials);
-
-    //compute(pTopo->collectPointsForAltimetrie(false), 1.E-5);
-
-    return pAlti;
+    return pSite;
 }
-
-static  const double precision = 1e-6;
 
 TEST(TYAltimetryTest, update_point_altitude)
 {
-    // Create altimetry
-    TYAltimetrie* pAlti = buildSlopeAltimetry();
+    LPTYProjet pProjet = new TYProjet();
+    LPTYSiteNode pSite = buildSiteSlopeAltimetry();
+    pProjet->setSite(pSite);
+    LPTYTopographie pTopo = pSite->getTopographie();
+    update_altimetry(pSite);
+    LPTYAltimetrie pAlti = pTopo->getAltimetrie();
 
     // Positive tests : Points lie in the convex hull of the altimetry
     OPoint3D pt(-70., 0., 0.);
     bool bRes = pAlti->updateAltitude(pt);
     EXPECT_NEAR(0.0, pt._z, precision);
-
     pt._x = 0.;
     bRes = pAlti->updateAltitude(pt);
     EXPECT_NEAR(150.0, pt._z, precision);
-
     pt._x = 70.;
     bRes = pAlti->updateAltitude(pt);
     EXPECT_NEAR(300.0, pt._z, precision);
@@ -98,6 +103,7 @@ LPTYSiteNode buildSiteSimpleAltimetry(void)
     LPTYSiteNode pSite = new TYSiteNode();
     pSite->setAltiEmprise(-100);
     pSite->setUseEmpriseAsCrbNiv(false);
+    // No landtake specified. default landtake has dimension 400X400m
 
 #define NB_POINTS_LEVEL_CURVE 5
     TYCourbeNiveau* pCrb = new TYCourbeNiveau();
@@ -159,16 +165,14 @@ LPTYCourbeNiveau addHillToSimpleSite(LPTYSiteNode pSite)
     return pCrb;
 }
 
-
 TEST(TYAltimetryTest, dummy_grid)
 {
+    LPTYProjet pProjet = new TYProjet();
     LPTYSiteNode pSite = buildSiteSimpleAltimetry();
+    pProjet->setSite(pSite);
     LPTYTopographie pTopo = pSite->getTopographie();
     LPTYAltimetrie pAlti = pTopo->getAltimetrie();
-
-    // Update the altimetry
-    pSite->altimetryNeedsUpdate();
-    ASSERT_TRUE(pSite->updateAltimetrie(true));
+    update_altimetry(pSite);
 
     // 2 triangles should give a 1x1 accelerating grid
     EXPECT_EQ(1, pAlti->_gridSX);
@@ -190,40 +194,38 @@ TEST(TYAltimetryTest, dummy_grid)
     EXPECT_DOUBLE_EQ(TYAltimetrie::invalid_altitude, pt._z);
 }
 
-
-
 TEST(TYAltimetryTest, simple_grid)
 {
+    LPTYProjet pProjet = new TYProjet();
     LPTYSiteNode pSite = buildSiteSimpleAltimetry();
+    pProjet->setSite(pSite);
     LPTYTopographie pTopo = pSite->getTopographie();
     LPTYAltimetrie pAlti = pTopo->getAltimetrie();
     LPTYCourbeNiveau pHill = addHillToSimpleSite(pSite);
+    update_altimetry(pSite);
 
-    // Update the altimetry and check the altitude of an inner point
-    pSite->altimetryNeedsUpdate();
-    ASSERT_TRUE(pSite->updateAltimetrie(true));
+    // 64 triangles should give a 4x4 accelerating grid
+    // (_gridSX = _gridSY = sqrt(nb triangles)/2)
+    EXPECT_EQ(64, pAlti->getListFaces().size());
+    EXPECT_EQ(4, pAlti->_gridSX);
+    EXPECT_EQ(4, pAlti->_gridSY);
 
-    // 10 triangles should give a 2x2 accelerating grid
-    EXPECT_EQ(10, pAlti->getListFaces().size());
-    EXPECT_EQ(2, pAlti->_gridSX);
-    EXPECT_EQ(2, pAlti->_gridSY);
-
-    // Check getGridIndices in the four quadrants
+    // Check getGridIndices in the four quadrants (landtake: [-200;200] on X and Y axes)
     TYAltimetrie::grid_index idx;
-    ASSERT_TRUE(pAlti->getGridIndices(OPoint3D(-10, -10, 0), idx));
+    ASSERT_TRUE(pAlti->getGridIndices(OPoint3D(-150, -150, 0), idx));
     EXPECT_EQ(0, idx.pi);
     EXPECT_EQ(0, idx.qi);
 
-    ASSERT_TRUE(pAlti->getGridIndices(OPoint3D(-10, 10, 0), idx));
+    ASSERT_TRUE(pAlti->getGridIndices(OPoint3D(-150, 150, 0), idx));
     EXPECT_EQ(0, idx.pi);
-    EXPECT_EQ(1, idx.qi);
+    EXPECT_EQ(3, idx.qi);
 
-    ASSERT_TRUE(pAlti->getGridIndices(OPoint3D(10, 10, 0), idx));
-    EXPECT_EQ(1, idx.pi);
-    EXPECT_EQ(1, idx.qi);
+    ASSERT_TRUE(pAlti->getGridIndices(OPoint3D(150, 150, 0), idx));
+    EXPECT_EQ(3, idx.pi);
+    EXPECT_EQ(3, idx.qi);
 
-    ASSERT_TRUE(pAlti->getGridIndices(OPoint3D(10, -10, 0), idx));
-    EXPECT_EQ(1, idx.pi);
+    ASSERT_TRUE(pAlti->getGridIndices(OPoint3D(150, -150, 0), idx));
+    EXPECT_EQ(3, idx.pi);
     EXPECT_EQ(0, idx.qi);
 
     // Check altitude in the middle of the hill
@@ -244,13 +246,12 @@ TEST(TYAltimetryTest, simple_grid)
 
 TEST(TYAltimetryTest, simple_terrain)
 {
+    LPTYProjet pProjet = new TYProjet();
     LPTYSiteNode pSite = buildSiteSimpleAltimetry();
+    pProjet->setSite(pSite);
     LPTYTopographie pTopo = pSite->getTopographie();
     LPTYAltimetrie pAlti = pTopo->getAltimetrie();
-
-    // Update the altimetry and check the altitude of an inner point
-    pSite->altimetryNeedsUpdate();
-    ASSERT_TRUE(pSite->updateAltimetrie(true));
+    update_altimetry(pSite);
 
     // Check altitude in the middle of the future terrain
     OPoint3D pt(10.0, 10.0, 0.0);
@@ -258,8 +259,6 @@ TEST(TYAltimetryTest, simple_terrain)
     EXPECT_NEAR(level_curve_A_alti, pt._z, precision);
 
     LPTYTerrain pTerrain = addTerrainToSimpleSite(pSite);
-    // Update the altimetry and check the altitude of an inner point
-    ASSERT_TRUE(pSite->updateAltimetrie(true));
 
     // Check again
     pt.setCoords(10.0, 10.0, 0.0);
