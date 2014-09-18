ad75aaca14c9d43ce721a1993d54a2d348cd3cc6
Laura M?dioni <laura.medioni@logilab.fr>
2014-09-18 11:20:16 +0200

    [solver config/cython] parse solver config from TYCAlcul and set it to the solver
    Move SOLVER_CONFIG_ATTRIBUTES declaration tympan as it is now also used in
    solve_project.py

--- tools/generate_solver_cy.py
+++ tools/generate_solver_cy.py
@@ -1,71 +1,7 @@
 """Generate solver.pxd/solver.pyx from solver.pxd.in/solver.pyx.in and by adding
 generated code to handle SolverConfiguration class
 """
-
-SOLVER_CONFIG_ATTRIBUTES = [
-        ('double', 'AtmosPressure'),
-        ('double', 'AtmosTemperature'),
-        ('double', 'AtmosHygrometry'),
-        ('double', 'AnalyticC0'),
-        ('double', 'WindDirection'),
-        ('double', 'AnalyticGradC'),
-        ('double', 'AnalyticGradV'),
-
-        ('int', 'RayTracingOrder'),
-        ('int', 'Discretization'),
-        ('int', 'NbRaysPerSource'),
-        ('float', 'MaxLength'),
-        ('float', 'SizeReceiver'),
-        ('int', 'Accelerator'),
-        ('int', 'MaxTreeDepth'),
-        ('float', 'AngleDiffMin'),
-        ('float', 'CylindreThick'),
-        ('int', 'MaxProfondeur'),
-        ('bool', 'UseSol'),
-        ('int', 'MaxReflexion'),
-        ('int', 'MaxDiffraction'),
-        ('bool', 'DiffractionUseRandomSampler'),
-        ('int', 'NbRayWithDiffraction'),
-        ('bool', 'DiffractionDropDownNbRays'),
-        ('bool', 'DiffractionFilterRayAtCreation'),
-        ('bool', 'UsePathDifValidation'),
-        ('float', 'MaxPathDifference'),
-        ('bool', 'DiffractionUseDistanceAsFilter'),
-        ('bool', 'KeepDebugRay'),
-        ('bool', 'UsePostFilters'),
-
-        ('bool', 'EnableFullTargets'),
-        ('float', 'TargetsDensity'),
-
-        ('float', 'InitialAngleTheta'),
-        ('float', 'InitialAnglePhi'),
-        ('int', 'AnalyticNbRay'),
-        ('double', 'AnalyticTMax'),
-        ('double', 'AnalyticH'),
-        ('double', 'AnalyticDMax'),
-
-        ('int', 'AnalyticTypeTransfo'),
-
-        ('int', 'NbThreads'),
-        ('bool', 'UseRealGround'),
-        ('bool', 'UseLateralDiffraction'),
-        ('bool', 'UseReflection'),
-        ('bool', 'PropaConditions'),
-        ('float', 'H1parameter'),
-        ('bool', 'ModSummation'),
-
-        ('bool', 'UseMeteo'),
-        ('float', 'OverSampleD'),
-        ('bool', 'UseFresnelArea'),
-        ('float', 'Anime3DSigma'),
-        ('int', 'Anime3DForceC'),
-
-        ('bool', 'DebugUseCloseEventSelector'),
-        ('bool', 'DebugUseDiffractionAngleSelector'),
-        ('bool', 'DebugUseDiffractionPathSelector'),
-        ('bool', 'DebugUseFermatSelector'),
-        ('bool', 'DebugUseFaceSelector'),
-]
+from tympan import SOLVER_CONFIG_ATTRIBUTES
 
 with open('solver.pxd', 'w') as output_stream:
     output_stream.write('''"""THIS FILE IS GENERATED, DON'T EDIT IT"""
