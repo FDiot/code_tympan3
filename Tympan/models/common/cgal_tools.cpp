d3535f82f35d750c00687687257063415f4fbe85
Laura M?dioni <laura.medioni@logilab.fr>
2015-05-20 08:50:43 +0200

    [solver model] Add functions allowing to intersect a triangle soup with a volume
    In common/cgal_tools goes all the CGAL-specific stuff. In solver/acoustic_problem_model goes the
    solver-model related code, allowing to build CGAL triangles from the scene.
    
    The implementation of the intersection code is inspired from the examples of CGAL
    documentation available at http://doc.cgal.org/4.4/Box_intersection_d/index.html (see parts 4 and 5)
    
    Regarding the "query", we artificially recreate a CGAL 3D box of the dimensions of the
    intersecting volume by creating a triangle whose iso-oriented bounding box will be the
    intersecting volume.
    
    related to #3176136

--- Tympan/models/common/cgal_tools.cpp
+++ Tympan/models/common/cgal_tools.cpp
@@ -8,6 +8,8 @@
  */
 
 #include <cassert>
+#include<algorithm>
+#include <functional>
 
 #include "cgal_tools.h"
 
@@ -19,6 +21,66 @@ CGAL_Plane to_cgal(const OPlan& oplan)
     CGAL_Point3 o(to_cgal(oplan.rframe._origin));
     CGAL_Vector3 n(to_cgal(oplan.rframe._vecK));
     return CGAL_Plane(o, n);
+}
+
+void intersection_report(std::deque<size_t>* intersected, CGAL_Triangles::iterator start_index,
+                         const CGAL_TBox& a, const CGAL_TBox& b)
+{
+    // XXX here we know the 2 boxes intersect, but we don't know for sure that the triangle of
+    // box a intersects with box b.
+    intersected->push_back(a.handle() - start_index);
+}
+
+// This implementation is inspired from http://doc.cgal.org/4.4/Box_intersection_d/index.html
+// (see parts "4 Minimal Example for Intersecting Boxes" and
+// "5 Example for Finding Intersecting 3D Triangles")
+std::deque<size_t> intersected_triangles(CGAL_Triangles & triangle_soup, OBox2 query)
+{
+    std::deque<CGAL_TBox> boxes;
+    // Put the triangles of the scene (triangle soup) in iso-oriented bounded boxes
+    for (CGAL_Triangles::iterator it = triangle_soup.begin(); it != triangle_soup.end(); it++)
+    {
+        boxes.push_back(CGAL_TBox(it->bbox(), it));
+    }
+    // Create a triangle whose bounding box will have the dimensions of the "query" box,
+    // choosing 3 nodes of the box as the triangle nodes (works because CGAL bounding box is
+    // iso-oriented)
+    CGAL_Triangles query_triangles;
+    query_triangles.push_back(CGAL_Triangle(CGAL_Point3(query._C._x, query._C._y, query._C._z),
+                CGAL_Point3(query._A._x, query._A._y, query._A._z),
+                CGAL_Point3(query._E._x, query._E._y, query._E._z)));
+    std::deque<CGAL_TBox> query_boxes;
+    CGAL::Bbox_3 box = query_triangles.begin()->bbox();
+    // Make sure the new query box has the same dimensions as "query" OBox2
+    double epsilon = 0.0001;
+    double x_dim = abs(box.xmax() - box.xmin());
+    double y_dim = abs(box.ymax() - box.ymin());
+    double z_dim = abs(box.zmax() - box.zmin());
+    assert(abs(x_dim - query._length) < epsilon
+            && "The dimensions of CGAL query box doesn't match query parameter.");
+    assert(abs(y_dim - query._width) < epsilon
+            && "The dimensions of CGAL query box doesn't match query parameter.");
+    assert(abs(z_dim - query._height) < epsilon
+            && "The dimensions of CGAL query box doesn't match query parameter.");
+    query_boxes.push_back(CGAL_TBox(query_triangles.begin()->bbox(), query_triangles.begin()));
+    // Compute intersection between the triangles from the triangle soup and the box
+    // from the query.
+    // "intersected" will contain the indices of the triangles of the triangle soup
+    // that intersect with the triangle of the query deque.
+    std::deque<size_t> intersected;
+    // use currying to specify container and triangle start index from here and therefore
+    // avoid using global variables (these variables must be accessible from the callback function,
+    // but CGAL::box_intersection_d requires a callback function that only deals with 2 boxes).
+    auto cgal_callback = std::bind(intersection_report, &intersected, triangle_soup.begin(),
+                                   std::placeholders::_1, std::placeholders::_2);
+    CGAL::box_intersection_d(boxes.begin(), boxes.end(), query_boxes.begin(), query_boxes.end(),
+                             cgal_callback);
+    // remove duplicates (first order the triangle ids since std::unique only works on
+    // an ordered container)
+    std::sort(intersected.begin(), intersected.end());
+    auto last = std::unique(intersected.begin(), intersected.end());
+    intersected.erase(last, intersected.end());
+    return intersected;
 }
 
 /* **** class PolygonTriangulator **** */
