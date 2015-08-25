cb98ad24b51f7fd132e8ca07a89ec2e884dbedaf
Bachir Hamrouche <bachir.hamrouche@edf.fr>
2015-08-25 18:47:00 +0200

    Correcting code display errors (indentations, spaces, empty lines ...etc)

--- Tympan/gui/widgets/TYSolWidget.cpp
+++ Tympan/gui/widgets/TYSolWidget.cpp
@@ -1,22 +1,22 @@
 /*
- * Copyright (C) <2012> <EDF-R&D> <FRANCE>
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU General Public License for more details.
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+* Copyright (C) <2012> <EDF-R&D> <FRANCE>
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+* See the GNU General Public License for more details.
+* You should have received a copy of the GNU General Public License along
+* with this program; if not, write to the Free Software Foundation, Inc.,
+* 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 */
 
 /**
- * \file TYSolWidget.cpp
- * \brief outil IHM pour un sol
- */
+* \file TYSolWidget.cpp
+* \brief outil IHM pour un sol
+*/
 
 
 //Added by qt3to4:
@@ -32,33 +32,33 @@
 
 
 TYSolWidget::TYSolWidget(TYSol* pElement, QWidget* _pParent /*=NULL*/):
-    TYWidget(pElement, _pParent)
+TYWidget(pElement, _pParent)
 {
-    _elmW = new TYElementWidget(pElement, this);
+	_elmW = new TYElementWidget(pElement, this);
 
-    resize(300, 300);
-    setWindowTitle(TR("id_caption"));
-    _solLayout = new QGridLayout();
-    setLayout(_solLayout);
+	resize(300, 300);
+	setWindowTitle(TR("id_caption"));
+	_solLayout = new QGridLayout();
+	setLayout(_solLayout);
 
-    _solLayout->addWidget(_elmW, 0, 0);
+	_solLayout->addWidget(_elmW, 0, 0);
 
-    _groupBox = new QGroupBox(this);
-    _groupBox->setTitle(TR(""));
-    _groupBoxLayout = new QGridLayout();
-    _groupBox->setLayout(_groupBoxLayout);
+	_groupBox = new QGroupBox(this);
+	_groupBox->setTitle(TR(""));
+	_groupBoxLayout = new QGridLayout();
+	_groupBox->setLayout(_groupBoxLayout);
 
-    _lineEditEpaisseur = new QLineEdit(_groupBox);
-    _groupBoxLayout->addWidget(_lineEditEpaisseur, 1, 1);
-    QLabel* pUnitEpais = new QLabel(_groupBox);
-    pUnitEpais->setText(TR("id_unite_epaisseur"));
-    _groupBoxLayout->addWidget(pUnitEpais, 1, 2);
+	_lineEditEpaisseur = new QLineEdit(_groupBox);
+	_groupBoxLayout->addWidget(_lineEditEpaisseur, 1, 1);
+	QLabel* pUnitEpais = new QLabel(_groupBox);
+	pUnitEpais->setText(TR("id_unite_epaisseur"));
+	_groupBoxLayout->addWidget(pUnitEpais, 1, 2);
 
-    _lineEditResistivite = new QLineEdit(_groupBox);
-    _groupBoxLayout->addWidget(_lineEditResistivite, 0, 1);
-    QLabel* pUnitResis = new QLabel(_groupBox);
-    pUnitResis->setText(TR("id_unite_resistivite"));
-    _groupBoxLayout->addWidget(pUnitResis, 0, 2);
+	_lineEditResistivite = new QLineEdit(_groupBox);
+	_groupBoxLayout->addWidget(_lineEditResistivite, 0, 1);
+	QLabel* pUnitResis = new QLabel(_groupBox);
+	pUnitResis->setText(TR("id_unite_resistivite"));
+	_groupBoxLayout->addWidget(pUnitResis, 0, 2);
 
 	_lineEditEcartType = new QLineEdit(_groupBox);
 	_groupBoxLayout->addWidget(_lineEditEcartType,2,1);
@@ -72,17 +72,17 @@ TYSolWidget::TYSolWidget(TYSol* pElement, QWidget* _pParent /*=NULL*/):
 	pUnitLong->setText(TR("id_unite_longueur"));
 	_groupBoxLayout->addWidget(pUnitLong,3,2);
 
-    _pushButtonResistivite = new QPushButton(_groupBox);
-    _pushButtonResistivite->setText(TR("id_edit_resistivite"));
-    _groupBoxLayout->addWidget(_pushButtonResistivite, 0, 3);
+	_pushButtonResistivite = new QPushButton(_groupBox);
+	_pushButtonResistivite->setText(TR("id_edit_resistivite"));
+	_groupBoxLayout->addWidget(_pushButtonResistivite, 0, 3);
 
-    _labelEpaisseur = new QLabel(_groupBox);
-    _labelEpaisseur->setText(TR("id_epaisseur_label"));
-    _groupBoxLayout->addWidget(_labelEpaisseur, 1, 0);
+	_labelEpaisseur = new QLabel(_groupBox);
+	_labelEpaisseur->setText(TR("id_epaisseur_label"));
+	_groupBoxLayout->addWidget(_labelEpaisseur, 1, 0);
 
-    _labelResistivite = new QLabel(_groupBox);
-    _labelResistivite->setText(TR("id_resistivite_label"));
-    _groupBoxLayout->addWidget(_labelResistivite, 0, 0);
+	_labelResistivite = new QLabel(_groupBox);
+	_labelResistivite->setText(TR("id_resistivite_label"));
+	_groupBoxLayout->addWidget(_labelResistivite, 0, 0);
 
 	_labelEcartType = new QLabel(_groupBox);
 	_labelEcartType->setText(TR("id_ecarttype_label"));
@@ -92,11 +92,11 @@ TYSolWidget::TYSolWidget(TYSol* pElement, QWidget* _pParent /*=NULL*/):
 	_labelLongueur->setText(TR("id_longueur_label"));
 	_groupBoxLayout->addWidget(_labelLongueur,3,0);
 
-    _solLayout->addWidget(_groupBox, 1, 0);
+	_solLayout->addWidget(_groupBox, 1, 0);
 
-    connect(_pushButtonResistivite, SIGNAL(clicked()), this, SLOT(editResistivite()));
+	connect(_pushButtonResistivite, SIGNAL(clicked()), this, SLOT(editResistivite()));
 
-    updateContent();
+	updateContent();
 }
 
 TYSolWidget::~TYSolWidget()
@@ -105,52 +105,52 @@ TYSolWidget::~TYSolWidget()
 
 void TYSolWidget::updateContent()
 {
-    QString num;
+	QString num;
 
-    _elmW->updateContent();
+	_elmW->updateContent();
 
-    _lineEditResistivite->setText(num.setNum(getElement()->getResistivite(), 'f', 2));
-    _lineEditEpaisseur->setText(num.setNum(getElement()->getEpaisseur(), 'f', 2));
+	_lineEditResistivite->setText(num.setNum(getElement()->getResistivite(), 'f', 2));
+	_lineEditEpaisseur->setText(num.setNum(getElement()->getEpaisseur(), 'f', 2));
 	_lineEditEcartType->setText(num.setNum(getElement()->getEcartType(),'f',2));
 	_lineEditLongueur->setText(num.setNum(getElement()->getLongueur(),'f',2));
 }
 
 void TYSolWidget::apply()
 {
-    _elmW->apply();
+	_elmW->apply();
 
-    // On ne peut pas rentrer une resistivite <= 0 (mini 1 kRayl)
-    double resistivite = _lineEditResistivite->text().toDouble() ;
-    resistivite = resistivite <= 1.0 ? 1.0 : resistivite;
-    getElement()->setResistivite(resistivite);
+	// On ne peut pas rentrer une resistivite <= 0 (mini 1 kRayl)
+	double resistivite = _lineEditResistivite->text().toDouble() ;
+	resistivite = resistivite <= 1.0 ? 1.0 : resistivite;
+	getElement()->setResistivite(resistivite);
 
-    // On ne peut avoir une epaisseur <= 0 (mini 1 cm)
-    double epaisseur = _lineEditEpaisseur->text().toDouble();
-    epaisseur = epaisseur <= 0.01 ? 0.01 : epaisseur;
-    getElement()->setEpaisseur(epaisseur);
+	// On ne peut avoir une epaisseur <= 0 (mini 1 cm)
+	double epaisseur = _lineEditEpaisseur->text().toDouble();
+	epaisseur = epaisseur <= 0.01 ? 0.01 : epaisseur;
+	getElement()->setEpaisseur(epaisseur);
 
 	double ecarttype = _lineEditEcartType->text().toDouble();
-    ecarttype = ecarttype < 0 ? 0 : ecarttype;
+	ecarttype = ecarttype < 0 ? 0 : ecarttype;
 	getElement()->setEcartType(ecarttype);
 
 	double longueur = _lineEditLongueur->text().toDouble();
 	longueur = longueur <=0 ? 0.0001: longueur;
 	getElement()->setLongueur(longueur);
 
-    emit modified();
+	emit modified();
 }
 
 void TYSolWidget::editResistivite()
 {
-    TYSolResistanceDialog* pDialog = new TYSolResistanceDialog(this, getElement()->getResistivite());
+	TYSolResistanceDialog* pDialog = new TYSolResistanceDialog(this, getElement()->getResistivite());
 
-    int ret = pDialog->exec();
+	int ret = pDialog->exec();
 
-    if (ret == QDialog::Accepted)
-    {
-        // Mise a jour du widget associe a la resistivite
-        _lineEditResistivite->setText(QString().setNum(pDialog->getResistivite(), 'f', 2));
-    }
+	if (ret == QDialog::Accepted)
+	{
+		// Mise a jour du widget associe a la resistivite
+		_lineEditResistivite->setText(QString().setNum(pDialog->getResistivite(), 'f', 2));
+	}
 }
 
 
--- Tympan/models/solver/acoustic_problem_model.cpp
+++ Tympan/models/solver/acoustic_problem_model.cpp
@@ -1,10 +1,10 @@
 /**
- * \file acoustic_problem_model.cpp
- * \brief Implementation for top-level of the model
- *
- * \date jul. 12 2013
- * \author Anthony Truchet <anthony.truchet@logilab.fr>
- */
+* \file acoustic_problem_model.cpp
+* \brief Implementation for top-level of the model
+*
+* \date jul. 12 2013
+* \author Anthony Truchet <anthony.truchet@logilab.fr>
+*/
 
 #include <iostream>
 #include <fstream>
@@ -19,116 +19,116 @@ using namespace std;
 namespace tympan
 {
 
-deque<triangle_idx> scene_volume_intersection(const triangle_pool_t & triangle_soup,
-                                              const nodes_pool_t & nodes, const OBox2 & vol)
-{
-    // Build CGAL triangles from the triangle soup of the scene
-    deque<CGAL_Point3> cgal_nodes;
-    for(nodes_pool_t::const_iterator it = nodes.begin(); it != nodes.end(); it ++)
-    {
-        cgal_nodes.push_back(CGAL_Point3(it->_x, it->_y, it->_z));
-    }
-    CGAL_Triangles cgal_triangles;
-    for(triangle_pool_t::const_iterator it = triangle_soup.begin(); it != triangle_soup.end(); it ++)
-    {
-        cgal_triangles.push_back(CGAL_Triangle(cgal_nodes[it->n[0]], cgal_nodes[it->n[1]],
-                                               cgal_nodes[it->n[2]]));
-    }
-    return intersected_triangles(cgal_triangles, vol);
-}
+	deque<triangle_idx> scene_volume_intersection(const triangle_pool_t & triangle_soup,
+		const nodes_pool_t & nodes, const OBox2 & vol)
+	{
+		// Build CGAL triangles from the triangle soup of the scene
+		deque<CGAL_Point3> cgal_nodes;
+		for(nodes_pool_t::const_iterator it = nodes.begin(); it != nodes.end(); it ++)
+		{
+			cgal_nodes.push_back(CGAL_Point3(it->_x, it->_y, it->_z));
+		}
+		CGAL_Triangles cgal_triangles;
+		for(triangle_pool_t::const_iterator it = triangle_soup.begin(); it != triangle_soup.end(); it ++)
+		{
+			cgal_triangles.push_back(CGAL_Triangle(cgal_nodes[it->n[0]], cgal_nodes[it->n[1]],
+				cgal_nodes[it->n[2]]));
+		}
+		return intersected_triangles(cgal_triangles, vol);
+	}
 
-node_idx AcousticProblemModel::make_node(const Point& p)
-{
-    all_nodes.push_back(p);
-    return all_nodes.size() - 1;
-}
+	node_idx AcousticProblemModel::make_node(const Point& p)
+	{
+		all_nodes.push_back(p);
+		return all_nodes.size() - 1;
+	}
 
-AcousticTriangle::AcousticTriangle(node_idx n1, node_idx n2, node_idx n3)
-{
-    n[0] = n1;
-    n[1] = n2;
-    n[2] = n3;
-}
+	AcousticTriangle::AcousticTriangle(node_idx n1, node_idx n2, node_idx n3)
+	{
+		n[0] = n1;
+		n[1] = n2;
+		n[2] = n3;
+	}
 
-/*
-bool AcousticProblemModel::less_than (const node_idx & lhs, const node_idx & rhs)
-{
-    if (lhs==rhs)
-        return false;
+	/*
+	bool AcousticProblemModel::less_than (const node_idx & lhs, const node_idx & rhs)
+	{
+	if (lhs==rhs)
+	return false;
 
-    const Point& lp = *lhs;
-    const Point& rp = *rhs;
+	const Point& lp = *lhs;
+	const Point& rp = *rhs;
 
-    return less_than(lp, rp);
-}
+	return less_than(lp, rp);
+	}
 
-bool AcousticProblemModel::less_than (const Point& lp, const Point& rp)
-{
-    if(lp._x == rp._x)
-    {
-        if(lp._y == rp._y)
-            return lp._z < rp._z;
-        else
-            return lp._y < rp._y;
-    }
-    else
-        return lp._x < rp._x;
-}
-*/
+	bool AcousticProblemModel::less_than (const Point& lp, const Point& rp)
+	{
+	if(lp._x == rp._x)
+	{
+	if(lp._y == rp._y)
+	return lp._z < rp._z;
+	else
+	return lp._y < rp._y;
+	}
+	else
+	return lp._x < rp._x;
+	}
+	*/
 
-triangle_idx AcousticProblemModel::make_triangle(node_idx n1, node_idx n2, node_idx n3)
-{
-    /*
-    AcousticProblemModel::node_idx tab[3] = {n1, n2, n3};
-    assert(tab[1] == n2);
-    assert(&tab[1] != &n2);
-    const unsigned k0 = std::min_element(&tab[0], &tab[3], AcousticProblemModel::less_than) - &tab[0];
-    assert( k0<3 );
+	triangle_idx AcousticProblemModel::make_triangle(node_idx n1, node_idx n2, node_idx n3)
+	{
+		/*
+		AcousticProblemModel::node_idx tab[3] = {n1, n2, n3};
+		assert(tab[1] == n2);
+		assert(&tab[1] != &n2);
+		const unsigned k0 = std::min_element(&tab[0], &tab[3], AcousticProblemModel::less_than) - &tab[0];
+		assert( k0<3 );
 
-    AcousticProblemModel::triangle tri;
-    // We fill the triangle array starting from k0
-    for(unsigned int k=k0; k<k0+3; k++)
-        tri.n[k%3] = tab[k];
-    return tri;
-    */
-    all_triangles.push_back(AcousticTriangle(n1, n2, n3));
-    return all_triangles.size() - 1;
-}
+		AcousticProblemModel::triangle tri;
+		// We fill the triangle array starting from k0
+		for(unsigned int k=k0; k<k0+3; k++)
+		tri.n[k%3] = tab[k];
+		return tri;
+		*/
+		all_triangles.push_back(AcousticTriangle(n1, n2, n3));
+		return all_triangles.size() - 1;
+	}
 
-material_ptr_t AcousticProblemModel::make_material(const string& name, double resistivity, double deviation, double length)
-{
-    material_ptr_t p_mat = tympan::static_pointer_cast<AcousticMaterialBase>(
-                               tympan::make_shared<AcousticGroundMaterial>(name, resistivity, deviation, length));
-    all_materials.push_back(p_mat);
-    return p_mat;
-}
+	material_ptr_t AcousticProblemModel::make_material(const string& name, double resistivity, double deviation, double length)
+	{
+		material_ptr_t p_mat = tympan::static_pointer_cast<AcousticMaterialBase>(
+			tympan::make_shared<AcousticGroundMaterial>(name, resistivity, deviation, length));
+		all_materials.push_back(p_mat);
+		return p_mat;
+	}
 
-material_ptr_t AcousticProblemModel::make_material(const string& name, const ComplexSpectrum& spectrum)
-{
-    material_ptr_t p_mat = tympan::static_pointer_cast<AcousticMaterialBase>(
-                               tympan::make_shared<AcousticBuildingMaterial>(name, spectrum));
-    all_materials.push_back(p_mat);
-    return p_mat;
-}
+	material_ptr_t AcousticProblemModel::make_material(const string& name, const ComplexSpectrum& spectrum)
+	{
+		material_ptr_t p_mat = tympan::static_pointer_cast<AcousticMaterialBase>(
+			tympan::make_shared<AcousticBuildingMaterial>(name, spectrum));
+		all_materials.push_back(p_mat);
+		return p_mat;
+	}
 
-source_idx AcousticProblemModel::make_source(
-        const Point& point,
-        const Spectrum& spectrum,
-        SourceDirectivityInterface* directivity)
-{
+	source_idx AcousticProblemModel::make_source(
+		const Point& point,
+		const Spectrum& spectrum,
+		SourceDirectivityInterface* directivity)
+	{
 
-    all_sources.push_back(AcousticSource(point, spectrum, directivity));
-    return all_sources.size() - 1;
-}
+		all_sources.push_back(AcousticSource(point, spectrum, directivity));
+		return all_sources.size() - 1;
+	}
 
-receptor_idx AcousticProblemModel::make_receptor(
-        const Point& position_)
-{
-    all_receptors.push_back(AcousticReceptor(position_));
-    return all_receptors.size() - 1;
-}
+	receptor_idx AcousticProblemModel::make_receptor(
+		const Point& position_)
+	{
+		all_receptors.push_back(AcousticReceptor(position_));
+		return all_receptors.size() - 1;
+	}
 
-    std::unique_ptr<AcousticProblemModel> make_AcousticProblemModel()
-    { return std::unique_ptr<AcousticProblemModel>(new AcousticProblemModel()); }
+	std::unique_ptr<AcousticProblemModel> make_AcousticProblemModel()
+	{ return std::unique_ptr<AcousticProblemModel>(new AcousticProblemModel()); }
 
 } // namespace tympan
