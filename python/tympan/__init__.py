59639eb4bd0e55fdca2f1449b39144ccbf7de405
Laura M?dioni <laura.medioni@logilab.fr>
2015-09-04 14:26:19 +0200

    [python API doc] add another usage example

--- python/tympan/__init__.py
+++ python/tympan/__init__.py
@@ -24,11 +24,35 @@ Here goes an example of the typical use that can be done of this API:
     # altitudes will be taken into account)
     model = Model.from_project(project)
     # Load and configure the acoustic solver
+    solverdir = os.environ.get('TYMPAN_SOLVERDIR')
     solver = Solver.from_project(project, solverdir)
     # Run the simulation
     result = solver.solve(model)
     # Update the project with the results of the computation
     project.import_result(model, result)
 
+This API can be used to do other types of computations, here goes an example of how to do a computation
+with user-defined sources:
+
+.. code-block:: python
+
+    # Build a project from a XML model, but don't build the sources
+    project = Project.from_xml("my_project.xml")
+    # same steps as described above
+    altimetry_mesh = AltimetryMesh.from_site(project.site)
+    project.update_site_altimetry(altimetry_mesh)
+    # build solver model, but don't use the project sources
+    model = Model.from_project(project, set_sources=False)
+    # Manually define the sources (position and spectrum) depending on your needs
+    model.add_source((0, 0, 0), np.array([100.0] * 31, dtype=float), 0)
+    model.add_source((100, 50, 0), np.array([150.0] * 31, dtype=float), 0)
+    tympan_solverdir = os.environ.get('TYMPAN_SOLVERDIR')
+    solver = Solver.from_project(project, ty_solverdir)
+    result = solver.solve(model)
+    project.import_result(model, result)
+    # Compute cumulate spectrum response value (quadratic pressure) from the sources for each receptor
+    cumulate_spec = []
+    for receptor_idx in xrange(model.nreceptors):
+        cumulate_spec.append(sum(result.spectrum(receptor_idx, source_idx).values for source_idx in (0, 1)))
 """
 
