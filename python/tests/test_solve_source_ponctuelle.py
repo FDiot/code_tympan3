8b4e3f1544879deda6e310d7d241c655ad88bfdf
Bachir HAMROUCHE <Bachir HAMROUCHE@localhost>
2015-12-23 12:19:19 +0100

    [Tests] Solves most expected failure tests except test_cube and test_solve_rayonnement_batiment

--- python/tests/test_solve_source_ponctuelle.py
+++ python/tests/test_solve_source_ponctuelle.py
@@ -4,7 +4,6 @@ from utils import TympanTC, _test_solve_with_file
 
 
 class TestSolveSourcePonctuelle(TympanTC):
-    @unittest.expectedFailure
     def test_source_ponctuelle(self):
         _test_solve_with_file('TEST_SOURCE_PONCTUELLE_NO_RESU.xml', self)
 
