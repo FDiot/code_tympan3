1b8d1df360f33f6238a7a2b9d3808708905beb0c
Pierre LEDAC <pierre.ledac@c-s.fr>
2017-03-23 17:42:42 +0100

    [AcousticRayTracer][#51] Fixes after running PyLint

--- python/tympan/models/acousticraytracer.py
+++ python/tympan/models/acousticraytracer.py
@@ -1,24 +1,24 @@
+"""Acoustic Ray Tracer module"""
 from tympan.models import _acousticraytracer as cyAcousticRayTracer
 
-
 class Simulation(object):
-    """Acoustic Ray Tracing.
+    """Main object Simulation for Ray Tracer
     """
     def __init__(self):
         """Create an empty simulation"""
         self._simulation = cyAcousticRayTracer.cySimulation()
 
-    def add_source(self, s):
+    def add_source(self, source):
         """Add a source (with default sampler)"""
-        self._simulation.addSource(s._source)
+        self._simulation.addSource(source.cysource)
 
-    def add_recepteur(self, r):
+    def add_recepteur(self, recepteur):
         """Add a receptor"""
-        self._simulation.addRecepteur(r._recepteur)
+        self._simulation.addRecepteur(recepteur.cyrecepteur)
 
-    def set_solver(self, s):
+    def set_solver(self, solver):
         """Add the solver"""
-        self._simulation.setSolver(s._solver)
+        self._simulation.setSolver(solver.cysolver)
 
     def set_accelerator(self):
         """Set the accelerator (by default, the ToDo one)"""
@@ -35,11 +35,11 @@ class Simulation(object):
     def clean(self):
         """Clean the process"""
         self._simulation.clean()
-    
+
     def configuration(self):
         """Return the ray tracer configuration"""
         return self._simulation.getConfiguration()
-    
+
     def export_scene(self, filename):
         """Export a Scene to a ply file"""
         self._simulation.export_to_ply(filename)
@@ -47,24 +47,23 @@ class Simulation(object):
     def import_scene(self, filename):
         """Import a Scene from a ply file"""
         self._simulation.import_from_ply(filename)
-    
+
 
 class Source(object):
     """Source"""
     def __init__(self, x=0, y=0, z=0):
         """Create an empty source"""
-        self._source = cyAcousticRayTracer.cySource(x,y,z)
-        
+        self.cysource = cyAcousticRayTracer.cySource(x, y, z)
 
 class Recepteur(object):
     """Receptor"""
     def __init__(self, x=0, y=0, z=0, r=0):
         """Create an empty receptor"""
-        self._recepteur = cyAcousticRayTracer.cyRecepteur(x,y,z,r)
+        self.cyrecepteur = cyAcousticRayTracer.cyRecepteur(x, y, z, r)
 
 
 class Solver(object):
     """Solver"""
     def __init__(self):
         """Create a solver"""
-        self._solver = cyAcousticRayTracer.cySolver()      
+        self.cysolver = cyAcousticRayTracer.cySolver()
