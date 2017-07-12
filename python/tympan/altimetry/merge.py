b9326d6fbf822940a0df81fb7ade550d49948704
Bachir HAMROUCHE <Bachir HAMROUCHE@localhost>
2017-07-12 23:13:27 +0200

    [Errors handling] More precisions in the overlapping areas message errors in altimetry calculation

--- python/tympan/altimetry/merge.py
+++ python/tympan/altimetry/merge.py
@@ -189,9 +189,10 @@ class SiteNodeGeometryCleaner(object):
         for i, area_id in enumerate(self._sorted_material_areas):
             area_geom, area_info = self[area_id]
             if inserted_area.shape.overlaps(area_geom):
-                msg = ("Partially overlapping material area in {subsite}")
+                msg = (
+                    "Partially overlapping material area in {subsite} between {ids} at positions {positions}")
                 raise InconsistentGeometricModel(msg, subsite=self.sitenode.id,
-                                                 ids=[area_id, inserted_area.id])
+                                                 ids=[area_id, inserted_area.id], positions=list(inserted_area.shape.intersection(area_geom).exterior.coords))
             if inserted_area.shape.within(area_geom):
                 return i
         else:
