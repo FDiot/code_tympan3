a816da7c55b7d908583884b24f2d6d94de3b823c
Bachir HAMROUCHE <Bachir HAMROUCHE@localhost>
2017-08-23 17:06:46 +0200

    [watch rays in DefaultSolver] Backout sur la r?vision : bddc67ffaab8

--- Tympan/solvers/DefaultSolver/TYEtape.cpp
+++ Tympan/solvers/DefaultSolver/TYEtape.cpp
@@ -31,7 +31,6 @@ TYEtape& TYEtape::operator=(const TYEtape& other)
     if (this != &other)
     {
         _pt = other._pt;
-        _type = other._type;
         _spectrum = other._spectrum;
     }
     return *this;
@@ -42,7 +41,6 @@ bool TYEtape::operator==(const TYEtape& other) const
     if (this != &other)
     {
         if (_pt != other._pt) { return false; }
-        if (_type != other._type) { return false; }
         if (_spectrum != other._spectrum) { return false; }
     }
     return true;
@@ -53,10 +51,8 @@ bool TYEtape::operator!=(const TYEtape& other) const
     return !operator==(other);
 }
 
-acoustic_event* TYEtape::asEvent() const
+acoustic_event* TYEtape::asEvent()
 {
     acoustic_event *returned_event = new acoustic_event();
-    returned_event->pos = _pt;
-    returned_event->type = _type;
     return returned_event;
 }
