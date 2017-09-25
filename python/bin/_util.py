a871d2cf4f20b3e9b18b1eef3861ab6e1944d3cc
Pierre LEDAC <pierre.ledac@c-s.fr>
2017-09-25 11:05:11 +0200

    [ToolBox][#64] Fix a Python warning (ResourceWarning)

--- python/bin/_util.py
+++ python/bin/_util.py
@@ -229,7 +229,11 @@ def ask_input_file(message):
 
 def line_count(filename):
     """ Count the lines number of a file """
-    return sum(1 for line in open(filename))
+    with open(filename) as f:
+        lines = sum(1 for _ in f)
+    f.close()
+    return lines
+    #return sum(1 for line in open(filename))
 
 
 def import_infra(object_file, object_type):
