d1e9b208773f17980ecaaeea1f4c1c8a8b878829
Olivier Giorgis <olivier.giorgis@logilab.fr>
2018-02-28 09:48:08 +0100

    Define a road profile
    Instead of passing all the arguments individually to the instantiation
    of a road, we prefer to pass a list of road profiles.
    This makes it possible to get closer to the business model and to have
    to control the coherence of the arguments between them.

--- python/tests/test_altimetry_roads.py
+++ python/tests/test_altimetry_roads.py
@@ -8,63 +8,39 @@ from tympan.altimetry import (
     datamodel,
 )
 
-road_coords = np.array([(5, 5, 1.2), (10, 5, 2), (10, 7, 3),
-                        (15, 8, 2.5), (15, 10, 2), (20, 13, 0.5)])
+road1_description = [
+    (5, 5, 1.2, 2, 2, 0, 10, 1, 1),
+    (10, 5, 2, 2, 2, 0, 10, 1, 1),
+    (10, 7, 3, 2, 2, 0, 10, 1, 1),
+    (15, 8, 2.5, 2, 2, 0, 10, 1, 1),
+    (15, 10, 2, 2, 2, 0, 10, 1, 1),
+    (20, 13, 0.5, 2, 2, 0, 10, 1, 1),
+]
+
+road2_description = [
+    (0, 15, 1, 2, 2, 0, 10, 0.2, 0.5),
+    (5, 15, 2, 2, 2, 0, 10, 0.2, 0.5),
+    (15, 15, 2.5, 2, 2, 0, 10, 0.2, 0.5),
+    (20, 15, 2, 2, 2, 0, 10, 0.2, 0.5),
+    (25, 15, 0.5, 2, 2, 0, 10, 0.2, 0.5),
+]
 
 
 class TestAltimetryRoads(unittest.TestCase):
 
     def test_add_road(self):
         asite = datamodel.SiteNode(rect(0, 0, 30, 30), id='Main site')
-        road = datamodel.Road(
-            road_coords[:, :2],
-            road_coords[:, 2],
-            width=(2, 2),
-            angle=(0, 10),
-            embankment=(1, 1),
-            id='road',
-        )
+        road_profiles = [datamodel.RoadProfile(*profile) for profile in
+                         road1_description]
+        road = datamodel.Road(road_profiles, id='road')
         asite.add_child(road)
         self.assertEqual(list(asite.roads)[0], road)
 
-    def test_create_bad_road(self):
-        with self.assertRaises(ValueError) as cm:
-            datamodel.Road(
-                road_coords[:, :2],
-                road_coords[:, 2][1:],
-                width=(2, 4),
-                angle=(0, -10),
-                id='road',
-            )
-        self.assertEqual(
-            str(cm.exception),
-            'coords and altitudes have different lengths for Road #road',
-        )
-
-    def test_create_bad_road_angle(self):
-        with self.assertRaises(ValueError) as cm:
-            datamodel.Road(
-                road_coords[:, :2],
-                road_coords[:, 2][1:],
-                width=(2, 4),
-                angle=0,
-                id='road',
-            )
-        self.assertEqual(
-            str(cm.exception),
-            'angle of Road #road is not a 2-tuple: (left value, rigth value)',
-        )
-
     def test_add_road_constraint(self):
         asite = datamodel.SiteNode(rect(0, 0, 30, 30), id="Main site")
-        road = datamodel.Road(
-            road_coords[:, :2],
-            road_coords[:, 2],
-            width=(4, 2),
-            angle=(10, 10),
-            id="road",
-            parent_site=asite,
-        )
+        road_profiles = [datamodel.RoadProfile(*profile) for profile in
+                         road1_description]
+        road = datamodel.Road(road_profiles, id='road', parent_site=asite)
         _, mesh, _ = builder.build_altimetry(asite)
         mesh = set(map(tuple, mesh.as_arrays()[0]))
         for line_coords in road.build_coordinates()[0]:
@@ -84,15 +60,9 @@ class TestAltimetryRoads(unittest.TestCase):
             parent_site=asite,
         )
         _, mesh, _ = builder.build_altimetry(asite)
-        road = datamodel.Road(
-            road_coords[:, :2],
-            road_coords[:, 2],
-            width=(2, 4),
-            angle=(0, 10),
-            embankment=(5, 2),
-            id="road",
-            parent_site=asite,
-        )
+        road_profiles = [datamodel.RoadProfile(*profile) for profile in
+                         road1_description]
+        road = datamodel.Road(road_profiles, id='road', parent_site=asite)
         merged_site, mesh_with_road, _ = builder.build_altimetry(asite)
         mesh = set(map(tuple, mesh_with_road.as_arrays()[0]))
         for line_coords in road.build_coordinates()[0]:
@@ -116,15 +86,9 @@ class TestAltimetryRoads(unittest.TestCase):
             id="level_curve",
             parent_site=asite,
         )
-        datamodel.Road(
-            road_coords[:, :2],
-            road_coords[:, 2],
-            width=(2, 2),
-            angle=(0, 10),
-            embankment=(1, 2),
-            id="road",
-            parent_site=asite,
-        )
+        road_profiles = [datamodel.RoadProfile(*profile) for profile in
+                         road1_description]
+        datamodel.Road(road_profiles, id='road', parent_site=asite)
         with self.assertRaises(NotImplementedError) as cm:
             builder.build_altimetry(asite)
         self.assertEqual(
@@ -143,24 +107,15 @@ class TestAltimetryRoads(unittest.TestCase):
             id="level_curve",
             parent_site=asite,
         )
-        road1 = datamodel.Road(
-            [(0, 15), (5, 15), (15, 15), (20, 15), (25, 15), (30, 15)],
-            road_coords[:, 2],
-            width=(2, 2),
-            angle=(0, 10),
-            embankment=(0.2, 0.5),
-            id="road1",
-            parent_site=asite,
-        )
-        road2 = datamodel.Road(
-            [(0, 20), (5, 20), (10, 20), (15, 20), (25, 20), (30, 20)],
-            road_coords[:, 2],
-            width=(2, 2),
-            angle=(0, 10),
-            embankment=(0.5, 0.2),
-            id="road2",
-            parent_site=asite,
-        )
+        road1_profiles = [datamodel.RoadProfile(*profile) for profile in
+                          road2_description]
+        road1 = datamodel.Road(road1_profiles, id='road1', parent_site=asite)
+        # make a road2 parallel to road1
+        road2_profiles = []
+        for profil in road1_profiles:
+            profil.coords = (profil.coords[0], profil.coords[1] + 5)
+            road2_profiles.append(profil)
+        road2 = datamodel.Road(road2_profiles, id='road2', parent_site=asite)
         merged_site, mesh_with_road, _ = builder.build_altimetry(asite)
         mesh = set(map(tuple, mesh_with_road.as_arrays()[0]))
         for line_coords in road1.build_coordinates()[0]:
